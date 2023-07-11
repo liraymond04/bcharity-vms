6211ae4d173030502be7c85b1fb975d82f7cbcd6
Ezra Huang <ezrahuang155@gmail.com>
2023-07-11 15:06:41 -0600

    add pagination

--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
@@ -1,31 +1,28 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useState } from 'react'
 
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
 import { defaultColumnDef, makeOrgColumnDefs } from './ColumnDefs'
 
+const makeFakeData = () => {
+  const data = []
+  for (let i = 0; i < 150; i++) {
+    data.push({
+      id: i,
+      activityName: `activity ${i}`,
+      vhr: {
+        current: i,
+        goal: 150
+      },
+      assigned: i
+    })
+  }
+  return data
+}
+
 const OrganizationCausesTab: React.FC = () => {
-  const [rowData] = useState([
-    {
-      id: '0',
-      activityName: 'activity 1',
-      vhr: { current: 100, goal: 100 },
-      assigned: 3
-    },
-    {
-      id: '1',
-      activityName: 'activity 2',
-      vhr: { current: 100, goal: 100 },
-      assigned: 3
-    },
-    {
-      id: '2',
-      activityName: 'activity 3',
-      vhr: { current: 99, goal: 100 },
-      assigned: 321
-    }
-  ])
+  const [rowData] = useState(makeFakeData())
 
   const onEdit = (id: string) => {
     console.log('edit id', id)
@@ -44,6 +41,7 @@ const OrganizationCausesTab: React.FC = () => {
           columnDefs={Object.values(
             makeOrgColumnDefs({ onEditClick: onEdit, onDeleteClick: onDelete })
           )}
+          pagination
         />
       </div>
     </div>
