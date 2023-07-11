7bb6ce1dec4a6a129318ec8bfb760d96e0285205
Ezra Huang <ezrahuang155@gmail.com>
2023-07-11 14:59:21 -0600

    preliminary grid code

---
+++ apps/app/src/components/Dashboard/OrganizationDashboard/ColumnDefs.tsx
@@ -0,0 +1,76 @@
+import { PencilIcon, TrashIcon } from '@heroicons/react/outline'
+import { CellClickedEvent } from 'ag-grid-community'
+
+export const defaultColumnDef = {
+  resizable: true
+}
+
+interface IColumnDefParams {
+  onEditClick: (id: string) => void
+  onDeleteClick: (id: string) => void
+}
+
+export const makeOrgColumnDefs: (params: IColumnDefParams) => any = (
+  params: IColumnDefParams
+) => {
+  return {
+    ['EditRow']: {
+      field: 'id',
+      headerName: '',
+      resizable: false,
+      cellRenderer: (_: any) => {
+        return <PencilIcon className="w-4 inline" />
+      },
+      onCellClicked: (event: CellClickedEvent) => {
+        params.onEditClick(event.data.id)
+      },
+      width: 50
+    },
+    ['DeleteRow']: {
+      field: 'id',
+      headerName: '',
+      resizable: false,
+      cellRenderer: (_: any) => {
+        return <TrashIcon className="w-4 inline" />
+      },
+      onCellClicked: (event: CellClickedEvent) => {
+        params.onDeleteClick(event.data.id)
+      },
+      width: 50
+    },
+    ['ActivityName']: {
+      field: 'activityName',
+      filter: 'agTextColumnFilter'
+    },
+    ['VHR']: {
+      field: 'vhr',
+      resizable: false,
+      valueFormatter: (_: any) => {
+        return '' // gets rid of ag-grid warning due to object data type, we use a custom cell renderer anyways
+      },
+      cellRenderer: (params: any) => {
+        console.log('value ', params.value)
+        return (
+          <div>
+            <meter
+              className="progress progress-success w-full"
+              value={params.value?.current}
+              min={0}
+              max={params.value?.goal}
+              low={Math.trunc(params.value?.goal / 2)}
+              high={params.value?.goal - 1}
+              optimum={params.value?.goal}
+            >
+              {/* <p>{`${params.value?.current} / ${params.value?.goal}`}</p> */}
+            </meter>
+          </div>
+        )
+      },
+      width: 110
+    },
+    ['Assigned']: {
+      field: 'assigned',
+      filter: 'agNumberColumnFilter'
+    }
+  }
+}
--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
+
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
+import { defaultColumnDef, makeOrgColumnDefs } from './ColumnDefs'
 
 const OrganizationCausesTab: React.FC = () => {
-  return <p>Organization Causes</p>
+  const [rowData] = useState([
+    {
+      id: '0',
+      activityName: 'activity 1',
+      vhr: { current: 100, goal: 100 },
+      assigned: 3
+    },
+    {
+      id: '1',
+      activityName: 'activity 2',
+      vhr: { current: 100, goal: 100 },
+      assigned: 3
+    },
+    {
+      id: '2',
+      activityName: 'activity 3',
+      vhr: { current: 99, goal: 100 },
+      assigned: 321
+    }
+  ])
+
+  const onEdit = (id: string) => {
+    console.log('edit id', id)
+  }
+
+  const onDelete = (id: string) => {
+    console.log('delete id', id)
+  }
+
+  return (
+    <div>
+      <div className="ag-theme-alpine" style={{ height: 600, width: 600 }}>
+        <AgGridReact
+          defaultColDef={defaultColumnDef}
+          rowData={rowData}
+          columnDefs={Object.values(
+            makeOrgColumnDefs({ onEditClick: onEdit, onDeleteClick: onDelete })
+          )}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default OrganizationCausesTab
