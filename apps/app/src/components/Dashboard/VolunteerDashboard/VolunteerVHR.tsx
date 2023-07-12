6f7e00d7a70f4319ebedfd0b5a13d4b2c1e64e26
liraymond04 <li.raymond04@gmail.com>
2023-07-12 16:07:35 -0600

    Add progress bar to VHR metrics

--- apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerVHR.tsx
+++ apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerVHR.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { useBalance } from 'wagmi'
 
@@ -11,6 +12,7 @@ const VolunteerVHRTab: React.FC = () => {
   const { isAuthenticated, currentUser } = useAppPersistStore()
 
   const [searchAddress, setSearchAddress] = useState<string>('')
+  const [vhrGoal, setVhrGoal] = useState(600) // use hardcoded goal for now
 
   const { data, isLoading } = useBalance({
     address: `0x${searchAddress.substring(2)}`,
@@ -23,7 +25,26 @@ const VolunteerVHRTab: React.FC = () => {
     }
   }, [currentUser, isAuthenticated])
 
-  console.log(data)
+  const Progress = ({
+    progress,
+    total,
+    className
+  }: {
+    progress: number
+    total: number
+    className?: string
+  }) => (
+    <div className={className}>
+      <div className="w-full bg-gray-200 rounded-full h-5 ">
+        <div
+          className="bg-green-400 h-5 rounded-full"
+          style={{
+            width: `${Math.min(Math.trunc((progress / total) * 100), 100)}%`
+          }}
+        ></div>
+      </div>
+    </div>
+  )
 
   return (
     <GridLayout>
@@ -33,14 +54,39 @@ const VolunteerVHRTab: React.FC = () => {
             {isLoading ? (
               <Spinner />
             ) : (
-              <div className="flex items-center">
-                <div className="text-2xl font-bold text-black sm:text-4xl">
-                  VHR Amount:
+              <>
+                <div className="flex items-center">
+                  <div className="text-2xl font-bold text-black sm:text-4xl">
+                    VHR Amount:
+                  </div>
+                  <div className="text-2xl font-extrabold text-black sm:text-7xl pl-10">
+                    {Number(data?.value)} / {vhrGoal}
+                  </div>
                 </div>
-                <div className="text-2xl font-extrabold text-black sm:text-7xl pl-10">
-                  {Number(data?.value)}
-                </div>
-              </div>
+                <Link
+                  href=""
+                  className="text-brand-500 hover:text-brand-600"
+                  onClick={() => {
+                    console.log('Set a goal')
+                  }}
+                >
+                  Set a goal
+                </Link>
+                <Progress
+                  progress={Number(data?.value)}
+                  total={vhrGoal}
+                  className="mt-10 mb-10"
+                />
+                {Number(data?.value) < vhrGoal ? (
+                  <div className="text-2xl font-normal text-black sm:text-2xl">
+                    {vhrGoal - Number(data?.value)} away from goal!
+                  </div>
+                ) : (
+                  <div className="text-2xl font-normal text-black sm:text-2xl">
+                    Reached goal!
+                  </div>
+                )}
+              </>
             )}
           </div>
         </Card>
