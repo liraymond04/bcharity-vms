0ebc0c93030bbd1b1b7c076332a4d17f117166ba
liraymond04 <li.raymond04@gmail.com>
2023-07-12 12:19:17 -0600

    Add user auth check for Dashboard in navbar

--- apps/app/src/components/Dashboard/index.tsx
+++ apps/app/src/components/Dashboard/index.tsx
@@ -1,10 +1,30 @@
+import { Spinner } from '@components/UI/Spinner'
 import SEO from '@components/utils/SEO'
 import { NextPage } from 'next'
+import Router from 'next/router'
+import { useEffect, useState } from 'react'
+
+import { useAppPersistStore } from '@/store/app'
 
 import OrganizationDashboard from './OrganizationDashboard/OrganizationDashboard'
 
 const Dashboard: NextPage = () => {
-  return (
+  const { isAuthenticated, currentUser } = useAppPersistStore()
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (isAuthenticated && currentUser) {
+      setLoading(false)
+    } else {
+      Router.push('/')
+    }
+  }, [currentUser, isAuthenticated])
+
+  return loading ? (
+    <center className="pt-20">
+      <Spinner />
+    </center>
+  ) : (
     <>
       <SEO title="Dashboard • BCharity VMS" />
       <OrganizationDashboard />
