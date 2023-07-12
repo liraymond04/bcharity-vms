5d41e322c92cd5702c194a1cb9175d8399605584
liraymond04 <li.raymond04@gmail.com>
2023-07-12 12:32:15 -0600

    Add check to display different dashboards for organizations and volunteers

--- apps/app/src/components/Dashboard/index.tsx
+++ apps/app/src/components/Dashboard/index.tsx
@@ -4,17 +4,22 @@ import { NextPage } from 'next'
 import Router from 'next/router'
 import { useEffect, useState } from 'react'
 
+import isVerified from '@/lib/isVerified'
 import { useAppPersistStore } from '@/store/app'
 
 import OrganizationDashboard from './OrganizationDashboard/OrganizationDashboard'
+import VolunteerDashboard from './VolunteerDashboard/VolunteerDashboard'
 
 const Dashboard: NextPage = () => {
   const { isAuthenticated, currentUser } = useAppPersistStore()
   const [loading, setLoading] = useState<boolean>(true)
 
+  const [isOrganization, setIsOrganization] = useState<boolean>(false)
+
   useEffect(() => {
     if (isAuthenticated && currentUser) {
       setLoading(false)
+      setIsOrganization(isVerified(currentUser.id))
     } else {
       Router.push('/')
     }
@@ -27,7 +32,7 @@ const Dashboard: NextPage = () => {
   ) : (
     <>
       <SEO title="Dashboard • BCharity VMS" />
-      <OrganizationDashboard />
+      {isOrganization ? <OrganizationDashboard /> : <VolunteerDashboard />}
     </>
   )
 }
