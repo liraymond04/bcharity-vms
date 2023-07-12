21b87e147ef8bbbe8346e2a721537696958d6987
liraymond04 <li.raymond04@gmail.com>
2023-07-12 15:25:11 -0600

    Add VHR metrics

--- apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerVHR.tsx
+++ apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerVHR.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useBalance } from 'wagmi'
+
+import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
+import { Card } from '@/components/UI/Card'
+import { Spinner } from '@/components/UI/Spinner'
+import { VHR_TOKEN } from '@/constants'
+import { useAppPersistStore } from '@/store/app'
 
 const VolunteerVHRTab: React.FC = () => {
-  return <p>Volunteer VHR</p>
+  const { isAuthenticated, currentUser } = useAppPersistStore()
+
+  const [searchAddress, setSearchAddress] = useState<string>('')
+  const [vhrBalance, setVhrBalance] = useState(0)
+
+  const { data, isError, isLoading } = useBalance({
+    address: `0x${searchAddress.substring(2)}`,
+    token: `0x${VHR_TOKEN.substring(2)}`
+  })
+
+  useEffect(() => {
+    if (isAuthenticated && currentUser) {
+      console.log(currentUser.ownedBy.substring(2))
+      setSearchAddress(currentUser.ownedBy)
+    }
+  }, [currentUser, isAuthenticated])
+
+  console.log(data)
+
+  return (
+    <GridLayout>
+      <GridItemTwelve>
+        <Card>
+          <div className="p-10 m-10">
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <div className="flex items-center">
+                <div className="text-2xl font-bold text-black sm:text-4xl">
+                  VHR Amount:
+                </div>
+                <div className="text-2xl font-extrabold text-black sm:text-7xl pl-10">
+                  {Number(data?.value)}
+                </div>
+              </div>
+            )}
+          </div>
+        </Card>
+      </GridItemTwelve>
+    </GridLayout>
+  )
 }
 
 export default VolunteerVHRTab
