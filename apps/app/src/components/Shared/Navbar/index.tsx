3a801c2094976f0907a934eb874f9f925811d9d4
Lithium Engineer <62958090+LithiumEngineer@users.noreply.github.com>
2023-07-13 09:35:40 -0600

    changed navbar features

--- apps/app/src/components/Shared/Navbar/index.tsx
+++ apps/app/src/components/Shared/Navbar/index.tsx
@@ -31,81 +31,83 @@ const Navbar: FC = () => {
     }
   }, [currentUser, isAuthenticated])
 
-  // interface NavItemProps {
-  //   url: string
-  //   name: string
-  //   current: boolean
-  // }
-
   return (
-    <div className="sticky z-20 top-0 flex justify-between bg-white bg-opacity-80 max-h-[16vh] border-b-2 border-gray-100">
+    <div className="sticky z-20 top-0 flex justify-between bg-white bg-opacity-80 max-h-[100vh] border-b-2 border-gray-100">
       <div className="flex items-center">
-        <img
-          className="m-5 h-10 w-10"
-          src="https://bcharity.vercel.app/logo.jpg "
-          alt="BCharity logo"
-        ></img>
-        <div className={`m-5 text-2xl text-violet-800 ${inter500.className}`}>
+        <a href="/">
+          <img
+            className="m-5 h-10 w-10"
+            src="https://bcharity.vercel.app/logo.jpg "
+            alt="BCharity logo"
+          ></img>
+        </a>
+        <div
+          className={`m-5 text-2xl text-violet-800 tracking-wider ${inter500.className}`}
+        >
           BCharity
         </div>
       </div>
-      <div className="flex w-[70vw] justify-around items-center">
-        <a href="/causes">
-          <button>
-            <div
-              className={`text-lg p-3 rounded-lg ${
-                pathname == '/causes'
-                  ? 'text-purple-900 bg-gray-200'
-                  : 'text-black'
-              } ${inter500.className}`}
-            >
-              CAUSES
-            </div>
-          </button>
-        </a>
-        <a href="/volunteers">
-          <button>
-            <div
-              className={`text-lg p-3 rounded-lg ${
-                pathname == '/volunteers'
-                  ? 'text-purple-900 bg-gray-200'
-                  : 'text-black'
-              } ${inter500.className}`}
-            >
-              VOLUNTEERS
-            </div>
-          </button>
-        </a>
-        <a href="/organizations">
-          <button>
-            <div
-              className={`text-lg p-3 rounded-lg ${
-                pathname == '/organizations'
-                  ? 'text-purple-900 bg-gray-200'
-                  : 'text-black'
-              } ${inter500.className}`}
-            >
-              ORGANIZATIONS
-            </div>
-          </button>
-        </a>
-        {auth && (
-          <a href="/dashboard">
+      <div className="flex">
+        <div className="flex w-[60] justify-around items-center">
+          <a href="/causes">
             <button>
               <div
-                className={`text-lg p-3 rounded-lg ${
-                  pathname == '/dashboard'
-                    ? 'text-purple-900 bg-gray-200'
+                className={`text-lg p-3 rounded-lg hover:bg-gray-100 tracking-wider ${
+                  pathname == '/causes'
+                    ? 'text-purple-500 bg-white'
                     : 'text-black'
                 } ${inter500.className}`}
               >
-                DASHBOARD
+                CAUSES
               </div>
             </button>
           </a>
-        )}
+          <a href="/volunteers">
+            <button>
+              <div
+                className={`text-lg p-3 rounded-lg hover:bg-gray-100 tracking-wider ${
+                  pathname == '/volunteers'
+                    ? 'text-purple-500 bg-white'
+                    : 'text-black'
+                } ${inter500.className}`}
+              >
+                VOLUNTEERS
+              </div>
+            </button>
+          </a>
+          <a href="/organizations">
+            <button>
+              <div
+                className={`text-lg p-3 rounded-lg hover:bg-gray-100 tracking-wider ${
+                  pathname == '/organizations'
+                    ? 'text-purple-500 bg-white'
+                    : 'text-black'
+                } ${inter500.className} ${auth ? '' : 'mr-10'}`}
+              >
+                ORGANIZATIONS
+              </div>
+            </button>
+          </a>
+          {auth && (
+            <a href="/dashboard">
+              <button>
+                <div
+                  className={`text-lg p-3 rounded-lg mr-10 hover:bg-gray-100 tracking-wider ${
+                    pathname == '/dashboard'
+                      ? 'text-purple-500 bg-white'
+                      : 'text-black'
+                  } ${inter500.className}`}
+                >
+                  DASHBOARD
+                </div>
+              </button>
+            </a>
+          )}
+        </div>
+        <div className="my-auto mr-10">
+          <MenuItems />
+        </div>
       </div>
-      <MenuItems />
     </div>
   )
 }
