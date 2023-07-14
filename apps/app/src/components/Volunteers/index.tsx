c4dccb8af36f87d281ff0ef9a04ebd4efbfb88f2
liraymond04 <li.raymond04@gmail.com>
2023-07-14 10:40:18 -0600

    Add UI for browse volunteer opportunities

--- apps/app/src/components/Volunteers/index.tsx
+++ apps/app/src/components/Volunteers/index.tsx
@@ -1,3 +1,6 @@
+import { GridItemFour, GridLayout } from '@components/GridLayout'
+import Search from '@components/Shared/Search'
+import { Card } from '@components/UI/Card'
 import SEO from '@components/utils/SEO'
 import { NextPage } from 'next'
 
@@ -5,7 +8,23 @@ const Volunteers: NextPage = () => {
   return (
     <>
       <SEO title="Volunteers • BCharity VMS" />
-      Volunteers
+      <div className="mx-auto max-w-screen-xl px-0 sm:px-5 font-bold text-2xl">
+        <div className="flex justify-between my-5">
+          <Search />
+        </div>
+        Browse volunteer opportunities
+      </div>
+      <GridLayout>
+        <GridItemFour>
+          <Card>Test</Card>
+        </GridItemFour>
+        <GridItemFour>
+          <Card>Test</Card>
+        </GridItemFour>
+        <GridItemFour>
+          <Card>Test</Card>
+        </GridItemFour>
+      </GridLayout>
     </>
   )
 }
