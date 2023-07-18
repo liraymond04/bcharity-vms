855e55040aa6ae5a90ce09212a879d99eea10a28
Matthew Zhang <matthew.zhang@ecssen.ca>
2023-07-18 12:57:29 -0600

    VM122

--- apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerCauses.tsx
+++ apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerCauses.tsx
@@ -1,7 +1,98 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { useBalance } from 'wagmi'
 
-const VolunteerCausesTab: React.FC = () => {
-  return <p>Volunteer Causes</p>
+import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
+import { Card } from '@/components/UI/Card'
+import { Spinner } from '@/components/UI/Spinner'
+import { VHR_TOKEN } from '@/constants'
+import { useAppPersistStore } from '@/store/app'
+
+const VolunteerCauses: React.FC = () => {
+  const { isAuthenticated, currentUser } = useAppPersistStore()
+
+  const [searchAddress, setSearchAddress] = useState<string>('')
+  const [donationGoal, setDonationsGoal] = useState(500) // use hardcoded goal for now
+
+  const { data, isLoading } = useBalance({
+    address: `0x${searchAddress.substring(2)}`,
+    token: `0x${VHR_TOKEN.substring(2)}`
+  })
+
+  useEffect(() => {
+    if (isAuthenticated && currentUser) {
+      setSearchAddress(currentUser.ownedBy)
+    }
+  }, [currentUser, isAuthenticated])
+
+  const Progress = ({
+    progress,
+    total,
+    className
+  }: {
+    progress: number
+    total: number
+    className?: string
+  }) => (
+    <div className={className}>
+      <div className="w-full bg-gray-200 rounded-full h-5 ">
+        <div
+          className="bg-green-400 h-5 rounded-full"
+          style={{
+            width: `${Math.min(Math.trunc((progress / total) * 100), 100)}%`
+          }}
+        ></div>
+      </div>
+    </div>
+  )
+
+  return (
+    <GridLayout>
+      <GridItemTwelve>
+        <Card>
+          <div className="p-10 m-10">
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <>
+                <div className="flex items-center">
+                  <div className="text-2xl font-bold text-black dark:text-white sm:text-4xl">
+                    Total donations:
+                  </div>
+                  <div className="text-2xl font-extrabold text-black dark:text-white sm:text-7xl pl-5">
+                    ${Number(data?.value)}
+                  </div>
+                </div>
+                <Link
+                  href=""
+                  className="text-brand-500 hover:text-brand-600"
+                  onClick={() => {
+                    console.log('Set a goal')
+                  }}
+                >
+                  Set a goal
+                </Link>
+                <Progress
+                  progress={Number(data?.value)}
+                  total={donationGoal}
+                  className="mt-10 mb-10"
+                />
+                {Number(data?.value) < donationGoal ? (
+                  <div className="text-2xl  font-semibold text-black dark:text-white sm:text-2x l">
+                    {donationGoal - Number(data?.value)} away from goal!
+                  </div>
+                ) : (
+                  <div className="text-2xl  font-semibold text-black dark:text-white sm:text-2xl">
+                    Reached goal!
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+        </Card>
+      </GridItemTwelve>
+    </GridLayout>
+  )
 }
 
-export default VolunteerCausesTab
+export default VolunteerCauses
