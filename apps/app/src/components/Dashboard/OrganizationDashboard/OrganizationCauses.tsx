674f934b1eea96e5693496d938f3ec8b8b1b4a65
Matthew Zhang <matthew.zhang@ecssen.ca>
2023-07-19 15:36:59 -0600

    VM800

--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
@@ -1,5 +1,4 @@
 import { PlusSmIcon } from '@heroicons/react/outline'
-import { FetchBalanceResult } from '@wagmi/core'
 import { AgGridReact } from 'ag-grid-react'
 import { useTheme } from 'next-themes'
 import React, { useEffect, useState } from 'react'
@@ -10,7 +9,6 @@ import { Spinner } from '@/components/UI/Spinner'
 import getCauseMetadata from '@/lib/lens-protocol/getCauseMetadata'
 import usePostData from '@/lib/lens-protocol/usePostData'
 import { PostTags } from '@/lib/types'
-import { useWalletBalance } from '@/lib/useBalance'
 import { useAppPersistStore } from '@/store/app'
 
 import Error from '../Modals/Error'
@@ -49,13 +47,7 @@ const OrganizationCauses: React.FC = () => {
     )
   }, [resolvedTheme])
   const { currentUser } = useAppPersistStore()
-  const [vhrGoal, setVhrGoal] = useState(600) // use hardcoded goal for now
 
-  const [data2, setData] = useState<FetchBalanceResult>()
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const { isLoading: isBalanceLoading, data: balanceData } = useWalletBalance(
-    currentUser?.ownedBy ?? ''
-  )
   const Progress = ({
     progress,
     total,
@@ -81,68 +73,6 @@ const OrganizationCauses: React.FC = () => {
     <GridLayout>
       <GridItemTwelve>
         <Card>
-          <div className="p-10 m-10">
-            {isLoading ? (
-              <Spinner />
-            ) : (
-              <>
-                <div className="flex items-center">
-                  <div className="text-3xl font-extrabold text-purple-500 dark:text-white sm:text-7xl pl-10 pr-3">
-                    {Number(balanceData?.value)}
-                  </div>
-                  <div className="text-2xl font-bold text-black dark:text-white sm:text-4xl">
-                    VHR raised out of {vhrGoal}
-                  </div>
-                </div>
-                <Progress
-                  progress={Number(balanceData?.value)}
-                  total={vhrGoal}
-                  className="mt-10 mb-10"
-                />
-                <div className="text-2xl font-bold text-black dark:text-white sm:text-4xl">
-                  Our Cause
-                </div>
-                <div className=" w-full lg:flex mt-5">
-                  <div className="border-r border-b border-l  p-5 lg:border-l-0 lg:border-t dark:border-x-stone-600 bg-white dark:bg-slate-500 rounded-b lg:rounded-b-none lg:rounded-r  flex flex-col justify-between leading-normal w-full">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Praesent dapibus, neque in auctor tincidunt, Lorem ipsum
-                    dolor sit amet, consectetur adipiscing elit. Praesent
-                    dapibus, neque in auctor tincidunt, tellus libero elementum
-                    nisl, vitae tristique eros lorem in odio. Nullam et eros
-                    sem. Duis molestie libero vel consequat suscipit. Sed
-                    maximus lacus vitae sem euismod ornare. In lacinia tempor
-                    lacus, vitae porta lectus luctus ac. Cras ultrices nulla eu
-                    enim ullamcorper iaculis. Nam gravida nibh sed sem interdum
-                    hendrerit. Nunc posuere purus id massa malesuada
-                    pellentesque. Etiam ipsum metus, laoreet eu libero a,
-                    suscipit sagittis ante. Nulla at purus consequat libero
-                    imperdiet efficitur quis quis orci. Aliquam felis orci,
-                    pretium sit amet volutpat ac, bibendum eu velit. Vivamus
-                    mollis, neque in aliquam malesuada, elit mi euismod velit,
-                    ac sagittis metus enim aliquet elit. Quisque fringilla
-                    sapien nec magna porta varius. Mauris bibendum, dui in
-                    dapibus bibendum, ex sapien ultricies lacus, a eleifend
-                    mauris erat sit amet purus.tellus libero elementum nisl,
-                    vitae tristique eros lorem in odio. Nullam et eros sem. Duis
-                    molestie libero vel consequat suscipit. Sed maximus lacus
-                    vitae sem euismod ornare. In lacinia tempor lacus, vitae
-                    porta lectus luctus ac. Cras ultrices nulla eu enim
-                    ullamcorper iaculis. Nam gravida nibh sed sem interdum
-                    hendrerit. Nunc posuere purus id massa malesuada
-                    pellentesque. Etiam ipsum metus, laoreet eu libero a,
-                    suscipit sagittis ante. Nulla at purus consequat libero
-                    imperdiet efficitur quis quis orci. Aliquam felis orci,
-                    pretium sit amet volutpat ac, bibendum eu velit. Vivamus
-                    mollis, neque in aliquam malesuada, elit mi euismod velit,
-                    ac sagittis metus enim aliquet elit. Quisque fringilla
-                    sapien nec magna porta varius. Mauris bibendum, dui in
-                    dapibus bibendum, ex sapien ultricies lacus, a eleifend
-                    mauris erat sit amet purus.
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
           <div className="p-5">
             <button
               onClick={onNew}
--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationVHR.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationVHR.tsx
@@ -2,16 +2,19 @@ import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
 
 import { PlusSmIcon } from '@heroicons/react/solid'
+import { FetchBalanceResult } from '@wagmi/core'
 import { AgGridReact } from 'ag-grid-react'
 import { useTheme } from 'next-themes'
 import React, { useEffect, useState } from 'react'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
+import Progress from '@/components/Shared/Progress'
 import { Card } from '@/components/UI/Card'
 import { Spinner } from '@/components/UI/Spinner'
 import getOpportunityMetadata from '@/lib/lens-protocol/getOpportunityMetadata'
 import usePostData from '@/lib/lens-protocol/usePostData'
 import { PostTags } from '@/lib/types'
+import { useWalletBalance } from '@/lib/useBalance'
 import { useAppPersistStore } from '@/store/app'
 
 import Error from '../Modals/Error'
@@ -92,10 +95,82 @@ const OrganizationVHRTab: React.FC = () => {
         }
       : { ...emptyPublishFormData }
   }
+
+  const { currentUser } = useAppPersistStore()
+  const [vhrGoal] = useState(600) // use hardcoded goal for now
+
+  const [data2, setData] = useState<FetchBalanceResult>()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const { isLoading: isBalanceLoading, data: balanceData } = useWalletBalance(
+    currentUser?.ownedBy ?? ''
+  )
   return (
     <GridLayout>
       <GridItemTwelve>
         <Card>
+          <div className="p-10 m-10">
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <>
+                <div className="flex items-center">
+                  <div className="text-3xl font-extrabold text-purple-500 dark:text-white sm:text-7xl pl-10 pr-3">
+                    {Number(balanceData?.value)}
+                  </div>
+                  <div className="text-2xl font-bold text-black dark:text-white sm:text-4xl">
+                    VHR raised out of {vhrGoal}
+                  </div>
+                </div>
+                <Progress
+                  progress={Number(balanceData?.value)}
+                  total={vhrGoal}
+                  className="mt-10 mb-10"
+                />
+                <div className="text-2xl font-bold text-black dark:text-white sm:text-4xl">
+                  Our Cause
+                </div>
+                <div className=" w-full lg:flex mt-5">
+                  <div className="border-r border-b border-l  p-5 lg:border-l-0 lg:border-t dark:border-x-stone-600 bg-white dark:bg-slate-500 rounded-b lg:rounded-b-none lg:rounded-r  flex flex-col justify-between leading-normal w-full">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent dapibus, neque in auctor tincidunt, Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. Praesent
+                    dapibus, neque in auctor tincidunt, tellus libero elementum
+                    nisl, vitae tristique eros lorem in odio. Nullam et eros
+                    sem. Duis molestie libero vel consequat suscipit. Sed
+                    maximus lacus vitae sem euismod ornare. In lacinia tempor
+                    lacus, vitae porta lectus luctus ac. Cras ultrices nulla eu
+                    enim ullamcorper iaculis. Nam gravida nibh sed sem interdum
+                    hendrerit. Nunc posuere purus id massa malesuada
+                    pellentesque. Etiam ipsum metus, laoreet eu libero a,
+                    suscipit sagittis ante. Nulla at purus consequat libero
+                    imperdiet efficitur quis quis orci. Aliquam felis orci,
+                    pretium sit amet volutpat ac, bibendum eu velit. Vivamus
+                    mollis, neque in aliquam malesuada, elit mi euismod velit,
+                    ac sagittis metus enim aliquet elit. Quisque fringilla
+                    sapien nec magna porta varius. Mauris bibendum, dui in
+                    dapibus bibendum, ex sapien ultricies lacus, a eleifend
+                    mauris erat sit amet purus.tellus libero elementum nisl,
+                    vitae tristique eros lorem in odio. Nullam et eros sem. Duis
+                    molestie libero vel consequat suscipit. Sed maximus lacus
+                    vitae sem euismod ornare. In lacinia tempor lacus, vitae
+                    porta lectus luctus ac. Cras ultrices nulla eu enim
+                    ullamcorper iaculis. Nam gravida nibh sed sem interdum
+                    hendrerit. Nunc posuere purus id massa malesuada
+                    pellentesque. Etiam ipsum metus, laoreet eu libero a,
+                    suscipit sagittis ante. Nulla at purus consequat libero
+                    imperdiet efficitur quis quis orci. Aliquam felis orci,
+                    pretium sit amet volutpat ac, bibendum eu velit. Vivamus
+                    mollis, neque in aliquam malesuada, elit mi euismod velit,
+                    ac sagittis metus enim aliquet elit. Quisque fringilla
+                    sapien nec magna porta varius. Mauris bibendum, dui in
+                    dapibus bibendum, ex sapien ultricies lacus, a eleifend
+                    mauris erat sit amet purus.
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+
           <div className="p-5">
             <button
               onClick={onNew}
