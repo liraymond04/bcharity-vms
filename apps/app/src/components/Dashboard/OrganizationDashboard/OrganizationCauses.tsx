5e2dee4a879ed7e9db54d8b3ff0e9cfaa5b7bd6c
Matthew Zhang <matthew.zhang@ecssen.ca>
2023-07-19 15:26:50 -0600

    VM800

--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
@@ -1,17 +1,57 @@
+import { PlusSmIcon } from '@heroicons/react/outline'
 import { FetchBalanceResult } from '@wagmi/core'
-import React, { useState } from 'react'
+import { AgGridReact } from 'ag-grid-react'
+import { useTheme } from 'next-themes'
+import React, { useEffect, useState } from 'react'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
 import { Card } from '@/components/UI/Card'
 import { Spinner } from '@/components/UI/Spinner'
+import getCauseMetadata from '@/lib/lens-protocol/getCauseMetadata'
+import usePostData from '@/lib/lens-protocol/usePostData'
+import { PostTags } from '@/lib/types'
 import { useWalletBalance } from '@/lib/useBalance'
 import { useAppPersistStore } from '@/store/app'
 
+import Error from '../Modals/Error'
+import PublishCauseModal from '../Modals/PublishCauseModal'
+import { defaultColumnDef, makeOrgCauseColumnDefs } from './ColumnDefs'
+
 const OrganizationCauses: React.FC = () => {
+  const { currentUser: profile } = useAppPersistStore()
+  const { resolvedTheme } = useTheme()
+  const [gridTheme, setGridTheme] = useState<string>()
+
+  const { data, error, loading, refetch } = usePostData({
+    profileId: profile!.id,
+    metadata: {
+      tags: { all: [PostTags.OrgPublish.Cause] }
+    }
+  })
+
+  const [publishModalOpen, setPublishModalOpen] = useState(false)
+
+  const onNew = () => {
+    setPublishModalOpen(true)
+  }
+
+  const onEdit = (id: string) => {
+    console.log('edit id ', id)
+  }
+
+  const onDelete = (id: string) => {
+    console.log('delete id ', id)
+  }
+
+  useEffect(() => {
+    setGridTheme(
+      resolvedTheme === 'light' ? 'ag-theme-alpine' : 'ag-theme-alpine-dark'
+    )
+  }, [resolvedTheme])
   const { currentUser } = useAppPersistStore()
   const [vhrGoal, setVhrGoal] = useState(600) // use hardcoded goal for now
 
-  const [data, setData] = useState<FetchBalanceResult>()
+  const [data2, setData] = useState<FetchBalanceResult>()
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const { isLoading: isBalanceLoading, data: balanceData } = useWalletBalance(
     currentUser?.ownedBy ?? ''
@@ -54,19 +94,7 @@ const OrganizationCauses: React.FC = () => {
                     VHR raised out of {vhrGoal}
                   </div>
                 </div>
-                import {PlusSmIcon} from '@heroicons/react/outline' import{' '}
-                {AgGridReact} from 'ag-grid-react' import {useTheme} from
-                'next-themes' import React, {(useEffect, useState)} from 'react'
-                import {(GridItemTwelve, GridLayout)} from
-                '@/components/GridLayout' import {Card} from
-                '@/components/UI/Card' import {Spinner} from
-                '@/components/UI/Spinner' import getCauseMetadata from
-                '@/lib/lens-protocol/getCauseMetadata' import usePostData from
-                '@/lib/lens-protocol/usePostData' import {PostTags} from
-                '@/lib/types' import {useAppPersistStore} from '@/store/app'
-                import Error from '../Modals/Error' import PublishCauseModal
-                from '../Modals/PublishCauseModal' import{' '}
-                {(defaultColumnDef, makeOrgCauseColumnDefs)} from './ColumnDefs'
+
                 <Progress
                   progress={Number(balanceData?.value)}
                   total={vhrGoal}
@@ -115,6 +143,47 @@ const OrganizationCauses: React.FC = () => {
                 </div>
               </>
             )}
+          </div>
+          <div className="p-5">
+            <button
+              onClick={onNew}
+              className="w-8 h-8 bg-purple-500 rounded-lg shadow-md border-black dark:border-white"
+            >
+              <PlusSmIcon className="w-8 text-white dark:text-black" />
+            </button>
+            <div
+              className={gridTheme}
+              style={{ height: '800px', width: '90%' }}
+            >
+              {loading ? (
+                <Spinner />
+              ) : (
+                <AgGridReact
+                  defaultColDef={defaultColumnDef}
+                  rowData={getCauseMetadata(data)}
+                  columnDefs={Object.values(
+                    makeOrgCauseColumnDefs({
+                      onEditClick: onEdit,
+                      onDeleteClick: onDelete
+                    })
+                  )}
+                  pagination
+                  paginationPageSize={20}
+                />
+              )}
+            </div>
+            {error && <Error message="An error occured. Please try again." />}
+            <PublishCauseModal
+              open={publishModalOpen}
+              onClose={(shouldRefetch) => {
+                setPublishModalOpen(false)
+
+                if (shouldRefetch) {
+                  refetch()
+                }
+              }}
+              publisher={profile}
+            />
           </div>
         </Card>
       </GridItemTwelve>
