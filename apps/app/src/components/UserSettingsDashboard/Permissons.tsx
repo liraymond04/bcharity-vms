66cec79ebf4137b16fb34f6470d83972708050a8
liraymond04 <li.raymond04@gmail.com>
2023-07-20 15:08:36 -0600

    Update UI of permissions page

--- apps/app/src/components/UserSettingsDashboard/Permissons.tsx
+++ apps/app/src/components/UserSettingsDashboard/Permissons.tsx
@@ -4,22 +4,20 @@ import {
   FollowModules,
   ReferenceModules
 } from '@lens-protocol/client'
-import {
-  prepareSendTransaction,
-  sendTransaction,
-  signMessage
-} from '@wagmi/core'
+import { prepareSendTransaction, sendTransaction } from '@wagmi/core'
 import clsx from 'clsx'
+import Link from 'next/link'
 import React, { FC, useEffect } from 'react'
 import { useState } from 'react'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
 import { Card } from '@/components/UI/Card'
-import HelpTooltip from '@/components/UI/HelpTooltip'
+import checkAuth from '@/lib/lens-protocol/checkAuth'
 import lensClient from '@/lib/lens-protocol/lensClient'
 import { useAppPersistStore } from '@/store/app'
 
 import { Button } from '../UI/Button'
+import HelpTooltip from '../UI/HelpTooltip'
 import { Spinner } from '../UI/Spinner'
 interface Props {
   currency: string
@@ -32,29 +30,27 @@ interface Props {
 const getResults = async (Option: string) => {
   return lensClient().modules.approvedAllowanceAmount({
     currencies: [Option],
-    collectModules: [CollectModules.LimitedFeeCollectModule],
+    collectModules: [
+      CollectModules.LimitedFeeCollectModule,
+      CollectModules.FeeCollectModule,
+      CollectModules.LimitedTimedFeeCollectModule,
+      CollectModules.TimedFeeCollectModule
+    ],
     followModules: [FollowModules.FeeFollowModule],
     referenceModules: [ReferenceModules.FollowerOnlyReferenceModule]
   })
 }
+
 const returnCollectModule = (module: string) => {
-  if (module === 'LimitedFeeCollectModule') {
-    return CollectModules.LimitedFeeCollectModule
-  }
-  return undefined
+  return CollectModules[module as keyof typeof CollectModules]
 }
 const returnFollowModule = (module: string) => {
-  if (module === 'FeeFollowModule') {
-    return FollowModules.FeeFollowModule
-  }
-  return undefined
+  return FollowModules[module as keyof typeof FollowModules]
 }
 const returnReferenceModule = (module: string) => {
-  if (module === 'FollowerOnlyReferenceModule') {
-    return ReferenceModules.FollowerOnlyReferenceModule
-  }
-  return undefined
+  return ReferenceModules[module as keyof typeof ReferenceModules]
 }
+
 const AllowanceButton: FC<Props> = ({
   currency,
   collectModule,
@@ -64,6 +60,10 @@ const AllowanceButton: FC<Props> = ({
 }) => {
   const [isCollectActive, setIsCollectActive] = useState(initValue !== '0x00')
   const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsCollectActive(initValue !== '0x00')
+  }, [initValue])
 
   const GenerateAllowance = async (value: string) => {
     const currencyData =
@@ -107,23 +107,9 @@ const AllowanceButton: FC<Props> = ({
         setIsLoading(false)
       }}
     >
-      {isLoading ? <Spinner /> : !isCollectActive ? '+ allow' : '- revoke'}
+      {isLoading ? <Spinner /> : !isCollectActive ? '+ Allow' : '- Revoke'}
     </Button>
   )
-}
-
-const auth = async (address: string) => {
-  const authenticated = await lensClient().authentication.isAuthenticated()
-
-  if (!authenticated) {
-    const challenge = await lensClient().authentication.generateChallenge(
-      address
-    )
-
-    const signature = await signMessage({ message: challenge })
-
-    await lensClient().authentication.authenticate(address, signature)
-  }
 }
 
 const Permissons: React.FC = () => {
@@ -138,103 +124,110 @@ const Permissons: React.FC = () => {
   >([])
 
   useEffect(() => {
-    if (isAuthenticated && currentUser) {
-      auth(currentUser.ownedBy)
+    if (currentUser) {
+      checkAuth(currentUser.ownedBy)
         .then(() => getResults(Option))
         .then((res) => {
-          console.log(res)
-
-          if (res.isSuccess()) {
+          if (res.isSuccess())
             setModuleData(res.unwrap() as ApprovedAllowanceAmountFragment[])
-          }
         })
         .catch((err) => console.log(err))
     }
   }, [currentUser, isAuthenticated, Option])
 
-  console.log(Option)
-
   return (
-    <p>
-      <GridLayout>
-        <GridItemTwelve>
-          <Card>
-            <div className="p-3">
-              <div className=" rounded-xl border-amber-950 border-width:3">
-                <div className="text-s font-bold text-black sm:text-s pd-2">
-                  Allow/Remove modules
-                </div>
-                <div className="text-m font-normal text-black p-2">
-                  In order to use the collect feature you need to allow the
-                  corresponding modules. You can allow and revoke permissions
-                  for modules at any time.
-                </div>
-                <div className="text-m font-normal text-black p-2">
-                  Select Currency:
-                </div>
-                <select onChange={(e) => setOptions(e.target.value)}>
-                  <option value="0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889">
-                    Wrapped Matic
-                  </option>
-
-                  <option value="0x3C68CE8504087f89c640D02d133646d98e64ddd9">
-                    WETH
-                  </option>
-
-                  <option value="0x2058A9D7613eEE744279e3856Ef0eAda5FCbaA7e">
-                    USDC
-                  </option>
-
-                  <option value="0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F">
-                    DAI
-                  </option>
-
-                  <option value="0x7beCBA11618Ca63Ead5605DE235f6dD3b25c530E">
-                    Toucan Protocal: Nature Carbon Tonne
-                  </option>
-                </select>
-                <div className="justify-center space-x-5 py-3">
-                  {moduleData.map((data, index) => {
-                    console.log(data, index)
-                    return (
-                      // eslint-disable-next-line react/jsx-key
-                      <div className="flex p-3">
-                        <Card key={index}>
-                          <div className="bg-white rounded-xl grid gap-2 md:grid-cols-2 lg:grid-cols-2">
-                            <div>
-                              <div className="flex text-s font-normal text-black p-2">
-                                {data.module}
-                                <HelpTooltip content="The Fee Collect Module allows for any follower to collect the associated publication provided they pay a fee set by the poster." />
+    <GridLayout>
+      <GridItemTwelve>
+        <Card>
+          <div className="p-3">
+            <div className=" rounded-xl border-amber-950 border-width:3">
+              <div className="text-s font-bold text-black sm:text-s p-2">
+                Allow/Remove modules
+              </div>
+              <div className="text-m font-normal text-black p-2">
+                In order to use the collect feature you need to allow the
+                corresponding modules. You can allow and revoke permissions for
+                modules at any time.
+              </div>
+              <div className="text-m font-normal text-black p-2">
+                Select Currency:
+              </div>
+              <select
+                className="ml-2 bg-white rounded-xl border border-gray-300 outline-none dark:bg-gray-800 disabled:bg-gray-500 disabled:bg-opacity-20 disabled:opacity-60 dark:border-gray-700/80 focus:border-brand-500 focus:ring-brand-400"
+                onChange={async (e) => {
+                  getResults(e.target.value)
+                    .then((res) => {
+                      // console.log(res)
+                      if (res.isSuccess())
+                        setModuleData(
+                          res.unwrap() as ApprovedAllowanceAmountFragment[]
+                        )
+                    })
+                    .catch((err) => console.log(err))
+                  setOptions(e.target.value)
+                }}
+              >
+                <option value="0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889">
+                  Wrapped Matic
+                </option>
+                <option value="0x3C68CE8504087f89c640D02d133646d98e64ddd9">
+                  WETH
+                </option>
+                <option value="0x2058A9D7613eEE744279e3856Ef0eAda5FCbaA7e">
+                  USDC
+                </option>
+                <option value="0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F">
+                  DAI
+                </option>
+                <option value="0x7beCBA11618Ca63Ead5605DE235f6dD3b25c530E">
+                  Toucan Protocal: Nature Carbon Tonne
+                </option>
+                flex
+              </select>
+              <div className="p-2 w-[75%]">
+                {moduleData.map((data, index) => {
+                  // console.log(data, index)
+                  return (
+                    <div className="mb-3" key={index}>
+                      <Card>
+                        <div className="bg-white rounded-xl flex justify-end p-2 items-center">
+                          <div className="px-2 pt-2 pb-1">
+                            <div className="flex text-s font-normal text-black">
+                              <div className="mr-1">{data.module}</div>
+                              <HelpTooltip content="The Fee Collect Module all p-1ows for any follower to collect the associated publication provided they pay a fee set by the poster." />
+                            </div>
+                            <Link
+                              href="https://mumbai.polygonscan.com/address/0xeb4f3EC9d01856Cec2413bA5338bF35CeF932D82"
+                              target="_blank"
+                            >
+                              <div className="text-s font-normal text-zinc-400">
+                                {data.contractAddress}
                               </div>
-                              <a href="https://mumbai.polygonscan.com/address/0xeb4f3EC9d01856Cec2413bA5338bF35CeF932D82">
-                                <div className="text-s font-normal text-zinc-400 p-1">
-                                  {data.contractAddress}
-                                </div>
-                              </a>
-                            </div>
-                            <div className="flex justify-end p-2">
-                              <AllowanceButton
-                                currency={data.currency}
-                                collectModule={returnCollectModule(data.module)}
-                                followModule={returnFollowModule(data.module)}
-                                referenceModule={returnReferenceModule(
-                                  data.module
-                                )}
-                                initValue={data.allowance}
-                              />
-                            </div>
+                            </Link>
                           </div>
-                        </Card>
-                      </div>
-                    )
-                  })}
-                </div>
+                          <div className="grow" />
+                          <div className="p-2">
+                            <AllowanceButton
+                              currency={data.currency}
+                              collectModule={returnCollectModule(data.module)}
+                              followModule={returnFollowModule(data.module)}
+                              referenceModule={returnReferenceModule(
+                                data.module
+                              )}
+                              initValue={data.allowance}
+                            />
+                          </div>
+                        </div>
+                      </Card>
+                    </div>
+                  )
+                })}
               </div>
             </div>
-          </Card>
-        </GridItemTwelve>
-      </GridLayout>
-    </p>
+          </div>
+        </Card>
+      </GridItemTwelve>
+    </GridLayout>
   )
 }
 
