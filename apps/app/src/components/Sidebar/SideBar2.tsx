5bd79a258cfc86eeb61fab2835e4f2096685e541
Michael Xi <michael@Michaels-MacBook-Air-3.local>
2023-07-20 15:53:48 -0600

    back button in profile settings now takes you to dashboard

--- apps/app/src/components/Sidebar/SideBar2.tsx
+++ apps/app/src/components/Sidebar/SideBar2.tsx
@@ -34,27 +34,29 @@ const Sidebar2: React.FC<ISidebarProps> = ({
   return auth && currentUser ? (
     <div className="w-sidebar from-violet-400 to-violet-300 dark:from-violet-800 dark:to-violet-500 bg-gradient-to-b min-h-screen">
       <div className="flex flex-row ...">
-        <button
-          type="button"
-          className="text-white ml-5 mt-5 mb-5 w-10 h-10 bg-opacity-0 bg-transparent hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-100 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-transparent dark:hover:bg-blue-200 dark:focus:ring-blue-300"
-        >
-          <svg
-            className="w-5 h-5 rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
+        <a href="../../../dashboard">
+          <button
+            type="button"
+            className="text-white ml-5 mt-5 mb-5 w-10 h-10 bg-opacity-0 bg-transparent hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-100 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-transparent dark:hover:bg-blue-200 dark:focus:ring-blue-300"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-          <span className="sr-only">Icon description</span>
-        </button>
+            <svg
+              className="w-5 h-5 rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+            <span className="sr-only">Icon description</span>
+          </button>
+        </a>
         <div className="ml-20 mr-12"></div>
         <img
           className=" w-10 h-10 mt-7 rounded-full"
