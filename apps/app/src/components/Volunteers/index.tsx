4e9ae2d1046c77da4c602186b71b1e609b095c35
Lithium Engineer <62958090+LithiumEngineer@users.noreply.github.com>
2023-07-21 10:56:31 -0600

    updated volunteers and organization filter UI

--- apps/app/src/components/Volunteers/index.tsx
+++ apps/app/src/components/Volunteers/index.tsx
@@ -1,6 +1,6 @@
 import { GridItemFour, GridLayout } from '@components/GridLayout'
-import Search from '@components/Shared/Search'
 import SEO from '@components/utils/SEO'
+import { SearchIcon } from '@heroicons/react/outline'
 import { PublicationSortCriteria } from '@lens-protocol/client'
 import { NextPage } from 'next'
 import { useEffect, useState } from 'react'
@@ -9,8 +9,8 @@ import getOpportunityMetadata from '@/lib/lens-protocol/getOpportunityMetadata'
 import useExplorePublications from '@/lib/lens-protocol/useExplorePublications'
 import { OpportunityMetadata, PostTags } from '@/lib/types'
 
+import DashboardDropDown from '../Dashboard/VolunteerDashboard/DashboardDropDown'
 import Divider from '../Shared/Divider'
-import FilterDropdown from '../Shared/FilterDropdown'
 import { Spinner } from '../UI/Spinner'
 import VolunteerCard from './VolunteerCard'
 
@@ -46,17 +46,44 @@ const Volunteers: NextPage = () => {
   return (
     <>
       <SEO title="Volunteers • BCharity VMS" />
-      <div className="mx-auto max-w-screen-xl px-0 sm:px-5 font-bold text-2xl">
-        <div className="flex justify-between py-5">
-          <Search searchText={searchValue} setSearchText={setSearchValue} />
-          <FilterDropdown
-            label="Filter:"
-            onChange={(c) => setSelectedCategory(c)}
-            options={Array.from(categories)}
-          />
+      <div className="mx-auto max-w-screen-xl px-0 sm:px-5">
+        <div className="flex flex-wrap gap-y-5 justify-around items-center mt-10">
+          <div className="flex justify-between w-[300px] h-[50px] bg-white items-center rounded-md border-violet-300 border-2 ml-10 mr-10">
+            <input
+              className="focus:ring-0 border-none outline-none focus:border-none focus:outline-none  bg-transparent rounded-2xl w-[250px]"
+              type="text"
+              value={searchValue}
+              placeholder="Search"
+              onChange={(e) => {
+                setSearchValue(e.target.value)
+              }}
+            />
+            <div className="h-5 w-5 mr-5">
+              <SearchIcon />
+            </div>
+          </div>
+
+          <div className="flex flex-wrap gap-y-5 justify-around w-[420px] items-center">
+            <div className="h-[50px] z-10 ">
+              <DashboardDropDown
+                label="Filter:"
+                options={Array.from(categories)}
+                onClick={(c) => setSelectedCategory(c)}
+                selected={selectedCategory}
+              ></DashboardDropDown>
+            </div>
+            <button
+              className="ml-3 min-w-[110px] h-fit text-red-500 bg-[#ffc2d4] border-red-500 border-2 rounded-md px-2 hover:bg-red-500 hover:text-white hover:cursor-pointer"
+              onClick={() => {
+                setSelectedCategory('')
+              }}
+            >
+              Clear Filters
+            </button>
+          </div>
         </div>
-        <Divider />
-        <p>Browse volunteer opportunities</p>
+        <Divider className="mt-5" />
+        <p className="font-bold text-2xl">Browse volunteer opportunities</p>
       </div>
       {loading ? (
         <div className="flex justify-center m-5">
