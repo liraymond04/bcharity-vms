752e1ce1ff330abedd79e42cb800e967278f6e85
liraymond04 <li.raymond04@gmail.com>
2023-07-24 11:42:53 -0600

    Add new avatar upload and fix profile pfp change

---
+++ apps/app/src/components/UserSettingsDashboard/UserHome/NFTPicture.tsx
@@ -0,0 +1,12 @@
+import { ProfileFragment } from '@lens-protocol/client'
+import { FC } from 'react'
+
+interface Props {
+  profile: ProfileFragment | undefined
+}
+
+const NFTPicture: FC<Props> = ({ profile }) => {
+  return <div />
+}
+
+export default NFTPicture
---
+++ apps/app/src/components/UserSettingsDashboard/UserHome/SelectAvatar.tsx
@@ -0,0 +1,86 @@
+import { PhotographIcon } from '@heroicons/react/outline'
+import { ProfileFragment } from '@lens-protocol/client'
+import clsx from 'clsx'
+import { FC, ReactNode, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { Card, CardBody } from '@/components/UI/Card'
+import { Spinner } from '@/components/UI/Spinner'
+import getProfile from '@/lib/lens-protocol/getProfile'
+import { useAppPersistStore } from '@/store/app'
+
+import NFTPicture from './NFTPicture'
+import Picture from './Picture'
+
+const SelectAvatar: FC = () => {
+  const { t } = useTranslation('common')
+  const { currentUser } = useAppPersistStore()
+
+  const [profile, _setProfile] = useState<ProfileFragment>()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [settingsType, setSettingsType] = useState<'NFT' | 'AVATAR'>('AVATAR')
+
+  useEffect(() => {
+    const _getProfile = async () => {
+      setIsLoading(true)
+      const _profile = await getProfile({ id: currentUser?.id ?? '' })
+      if (_profile) _setProfile(_profile)
+      setIsLoading(false)
+    }
+
+    _getProfile()
+  }, [currentUser?.id])
+
+  interface TypeButtonProps {
+    name: string
+    icon: ReactNode
+    type: 'NFT' | 'AVATAR'
+  }
+
+  const TypeButton: FC<TypeButtonProps> = ({ name, icon, type }) => (
+    <button
+      type="button"
+      onClick={() => {
+        setSettingsType(type)
+      }}
+      className={clsx(
+        {
+          'text-brand bg-brand-100 dark:bg-opacity-20 bg-opacity-100 font-bold':
+            settingsType === type
+        },
+        'flex items-center space-x-2 rounded-lg px-4 sm:px-3 py-2 sm:py-1 text-brand hover:bg-brand-100 dark:hover:bg-opacity-20 hover:bg-opacity-100'
+      )}
+    >
+      {icon}
+      <div className="hidden sm:block">{name}</div>
+    </button>
+  )
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <Card>
+      <CardBody className="space-y-5">
+        <div className="flex items-center space-x-2">
+          <TypeButton
+            icon={<PhotographIcon className="w-5 h-5" />}
+            type="AVATAR"
+            name={t('Upload avatar')}
+          />
+          <TypeButton
+            icon={<PhotographIcon className="w-5 h-5" />}
+            type="NFT"
+            name={t('NFT Avatar')}
+          />
+        </div>
+        {settingsType === 'NFT' ? (
+          <NFTPicture profile={profile} />
+        ) : (
+          <Picture profile={profile} />
+        )}
+      </CardBody>
+    </Card>
+  )
+}
+
+export default SelectAvatar
