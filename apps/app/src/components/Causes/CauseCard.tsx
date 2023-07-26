a87f026c548ef83827d25dffe7059dfc98acfaa3
Ezra Huang <ezrahuang155@gmail.com>
2023-07-26 13:30:02 -0600

    Cause card styling

--- apps/app/src/components/Causes/CauseCard.tsx
+++ apps/app/src/components/Causes/CauseCard.tsx
@@ -26,8 +26,8 @@ const CauseCard: React.FC<ICauseCardProps> = ({ cause }) => {
       return (
         <img
           src={getAvatar(cause.from)}
-          alt="Volunteer opportunity related image"
-          className="h-full w-auto m-auto"
+          alt="Organization avatar"
+          className="h-full w-auto"
         />
       )
     } else if (!resolvedImageUrl) {
@@ -41,23 +41,25 @@ const CauseCard: React.FC<ICauseCardProps> = ({ cause }) => {
         <img
           src={resolvedImageUrl}
           alt="Volunteer opportunity related image"
-          className="h-full w-auto m-auto"
+          className="object-cover h-full w-full"
         />
       )
     }
   }
 
   return (
-    <Card className="w-[300px] h-[350px] my-5 p-2 flex flex-col items-stretch justify-between">
-      <div className="flex flex-col items-stretch">
-        <div>{getDisplayedImage()}</div>
-        <p>{cause.location}</p>
-        <p>{cause.name}</p>
-        <p className="overflow-ellipsis">{cause.description}</p>
-        <p>Last donation X minutes ago</p>
-        <div>
+    <Card className="w-80 h-96 my-5 p-2 flex flex-col items-stretch justify-between">
+      <div className="flex flex-col items-stretch h-full">
+        <div className="w-full h-[200px]">{getDisplayedImage()}</div>
+        <p className="text-lg text-brand-600 font-semibold leading-7 mt-2">
+          {cause.location ? cause.location.toUpperCase() : 'CALGARY, AB'}
+        </p>
+        <p className="text-lg font-semibold leading-7">{cause.name}</p>
+        <p className="text-lg grow overflow-auto">{cause.description}</p>
+        <div className="mt-auto">
+          <p className="text-zinc-500 text-xs">Last donation X minutes ago</p>
           <Progress progress={10000} total={10000} />
-          <p>$10000 raised out of $10000</p>
+          <p className="text-sm font-bold">$10000 raised out of $10000</p>
         </div>
       </div>
     </Card>
