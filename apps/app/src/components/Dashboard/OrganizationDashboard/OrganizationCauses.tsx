449bc4039af967a3e6c8605721e6bc5de39fa6fb
liraymond04 <li.raymond04@gmail.com>
2023-07-27 16:58:40 -0600

    Fix redundant auth checks in fundraiser's tab

--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationCauses.tsx
@@ -2,11 +2,13 @@ import { PlusSmIcon } from '@heroicons/react/outline'
 import { AgGridReact } from 'ag-grid-react'
 import { useTheme } from 'next-themes'
 import React, { useEffect, useState } from 'react'
+import { toast } from 'react-hot-toast'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
 import { Card } from '@/components/UI/Card'
 import { Spinner } from '@/components/UI/Spinner'
 import getCauseMetadata from '@/lib/lens-protocol/getCauseMetadata'
+import useEnabledCurrencies from '@/lib/lens-protocol/useEnabledCurrencies'
 import usePostData from '@/lib/lens-protocol/usePostData'
 import { CauseMetadata, PostTags } from '@/lib/types'
 import { useWalletBalance } from '@/lib/useBalance'
@@ -91,6 +93,16 @@ const OrganizationCauses: React.FC = () => {
   const { isLoading: isBalanceLoading, data: balanceData } = useWalletBalance(
     currentUser?.ownedBy ?? ''
   )
+
+  const { data: currencyData, error: currencyError } = useEnabledCurrencies(
+    currentUser?.ownedBy
+  )
+
+  useEffect(() => {
+    if (currencyError) {
+      toast.error(currencyError.message)
+    }
+  }, [currencyError])
 
   const [vhrGoal] = useState(600) // use hardcoded goal for now
 
@@ -232,6 +244,7 @@ const OrganizationCauses: React.FC = () => {
               open={publishModalOpen}
               onClose={onPublishClose}
               publisher={profile}
+              currencyData={currencyData}
             />
             <DeleteCauseModal
               open={deleteModalOpen}
@@ -247,6 +260,7 @@ const OrganizationCauses: React.FC = () => {
               publisher={profile}
               id={currentModifyId}
               defaultValues={getFormDefaults(currentModifyId)}
+              currencyData={currencyData}
             />
           </div>
         </Card>
--- apps/app/src/lib/lens-protocol/useEnabledCurrencies.ts
+++ apps/app/src/lib/lens-protocol/useEnabledCurrencies.ts
@@ -6,29 +6,40 @@ import lensClient from './lensClient'
 
 const useEnabledCurrencies = (address: string | undefined) => {
   const [data, setData] = useState<Erc20Fragment[]>()
+  const [error, setError] = useState<Error>()
 
+  const fetch = async () => {
+    if (!address) return
+
+    await checkAuth(address)
+
+    const result = await lensClient().modules.fetchEnabledCurrencies()
+
+    return result
+  }
+
+  let run = true
   useEffect(() => {
     const getEnabledCurrencies = async () => {
       try {
-        if (!address) return
-        await checkAuth(address)
-        const authenticated =
-          await lensClient().authentication.isAuthenticated()
-        if (authenticated) {
-          const result = await lensClient().modules.fetchEnabledCurrencies()
-          return result
-        }
+        await fetch().then((result) => {
+          if (result) setData(result.unwrap())
+        })
       } catch (e) {
-        console.log(e)
+        if (e instanceof Error) {
+          setError(e)
+        }
       }
     }
 
-    getEnabledCurrencies().then((data) => {
-      if (data) setData(data.unwrap())
-    })
+    if (address && run) {
+      console.log(run)
+      getEnabledCurrencies()
+      run = false
+    }
   }, [address])
 
-  return { data }
+  return { data, error }
 }
 
 export default useEnabledCurrencies
