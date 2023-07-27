87b4ec25b96a04c25784ae04949bcec3301fa1a9
Ezra Huang <ezrahuang155@gmail.com>
2023-07-27 06:04:32 -0600

    format location properly

---
+++ apps/app/src/lib/lens-protocol/formatLocation.ts
@@ -0,0 +1,19 @@
+import { Country, State } from 'country-state-city'
+
+/**
+ * @brief Helper function to format metadata location strings
+ * @param locationString string in the format country-province-city
+ *
+ */
+export const formatLocation = (locationString: string) => {
+  console.log(locationString)
+  const [country, province, city] = locationString.split('-', 3)
+
+  const countryCode =
+    Country.getAllCountries().find((c) => c.name === country)?.isoCode ?? ''
+  const provinceCode = State.getStatesOfCountry(countryCode).find(
+    (p) => p.name === province
+  )?.isoCode
+
+  return `${city}, ${provinceCode}`.toUpperCase()
+}
