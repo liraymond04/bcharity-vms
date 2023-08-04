e5157fc8a532e29c25291e2fa9b852192727ee66
liraymond04 <li.raymond04@gmail.com>
2023-08-04 14:26:21 -0600

    Use node-fetch to make get request

--- apps/app/src/pages/api/cors.ts
+++ apps/app/src/pages/api/cors.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import fetch from 'node-fetch'
 
 type Data = string
 
@@ -11,7 +12,6 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse<Data>
 ) {
-  const https = require('https')
   let query = Object.entries(request.query)
   query.shift()
   let url = request.query.url
@@ -23,37 +23,20 @@ export default async function handler(
 
   response.status(status).send(data)
 
-  function getRequest(url: string | string[] | undefined) {
-    return new Promise<Response>((resolve) => {
-      const options = {
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
-        }
+  async function getRequest(url: string | string[] | undefined) {
+    const options = {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
       }
+    }
 
-      const req = https.get(
-        url,
-        options,
-        (resp: {
-          on: (
-            arg0: string,
-            arg1: { (): void } | { (chunk: string): void }
-          ) => void
-          statusCode: any
-        }) => {
-          let data = ''
-          resp.on('data', (chunk: string) => {
-            data += chunk
-          })
-          resp.on('end', () => {
-            resolve({
-              status: resp.statusCode,
-              data: data
-            })
-          })
-        }
-      )
-    })
+    const response = await fetch(`${url}`, options)
+    const data = await response.text()
+
+    return {
+      status: response.status,
+      data: data
+    }
   }
 }
