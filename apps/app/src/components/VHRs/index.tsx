a4d40ba5d6b22722ddbe58ca1f1a618f7a741cf0
liraymond04 <li.raymond04@gmail.com>
2023-08-04 13:14:12 -0600

    Move column to new component Add page SEO

--- apps/app/src/components/VHRs/index.tsx
+++ apps/app/src/components/VHRs/index.tsx
@@ -7,9 +7,11 @@ import { VHR_TOP_HOLDERS_URL } from '@/constants'
 import isVerified from '@/lib/isVerified'
 import getProfilesOwnedBy from '@/lib/lens-protocol/getProfilesOwnedBy'
 
-import { Spinner } from '../UI/Spinner'
+import { GridItemTwelve, GridLayout } from '../GridLayout'
+import SEO from '../utils/SEO'
+import Column from './Column'
 
-interface Item {
+export interface Item {
   index: number
   address: string
   handle: string
@@ -97,99 +99,32 @@ const VHRs: NextPage = () => {
   }, [])
 
   return (
-    <div className={`${inter500.className}`}>
-      <div className="flex justify-around items-center">
-        <div className="text-3xl text-[#343434] mt-10 dark:text-[#E2E2E2]">
-          Top VHR Holders
-        </div>
-      </div>
-      <div className="flex justify-around mb-20 flex-wrap">
-        <div className="min-w-[450px] mx-2 h-[550px] bg-[#F9F9F9] dark:bg-[#18004A] rounded-lg mt-10">
-          <div className="text-2xl text-[#626262] dark:text-[#E2E2E2] my-5 mx-auto w-fit h-fit">
-            Volunteers
-          </div>
-          <div className="overflow-y-scroll h-[470px]">
-            {volunteerData &&
-              volunteerData.map((value, index) => (
-                <div
-                  key={index}
-                  className="flex justify-between items-center w-[400px] h-[80px] mx-auto my-2 border-b-[1px] border-[#DDDDDD]"
-                >
-                  <div className="flex justify-between items-center">
-                    <div className="ml-2 text-[#7E7E7E]">{index + 1}</div>
-                    <div className="w-10 h-10 bg-[#498ade] mx-4 rounded-full"></div>
-                    <a href={`/user/${value.handle}`} target="_blank">
-                      <div
-                        className={`${
-                          index == 0
-                            ? 'text-[#FFD600]'
-                            : index == 1
-                            ? 'text-[#CCCCCC]'
-                            : index == 2
-                            ? 'text-[#8E5E00]'
-                            : 'text-[#A3A3A3]'
-                        } `}
-                      >
-                        {value.handle}
-                      </div>
-                    </a>
-                  </div>
-                  <div className="text-[#A3A3A3] text-sm">
-                    {value.amount} VHRs
-                  </div>
-                </div>
-              ))}
-            {volunteersIsLoading && (
-              <div className="flex justify-between items-center w-[400px] h-[80px] mx-auto my-2 border-[#DDDDDD]">
-                <Spinner />
+    <>
+      <SEO title="VHRs • BCharity VMS" />
+      <GridLayout>
+        <GridItemTwelve>
+          <div className={`${inter500.className}`}>
+            <div className="flex justify-around items-center">
+              <div className="text-3xl text-[#343434] mt-10 dark:text-[#E2E2E2]">
+                Top VHR Holders
               </div>
-            )}
+            </div>
+            <div className="flex justify-around mb-20 flex-wrap">
+              <Column
+                label="Volunteers"
+                data={volunteerData}
+                isLoading={volunteersIsLoading}
+              />
+              <Column
+                label="Organizations"
+                data={organizationData}
+                isLoading={organizationsIsLoading}
+              />
+            </div>
           </div>
-        </div>
-        <div className="min-w-[450px] mx-2 h-[550px] bg-[#F9F9F9] dark:bg-[#18004A] rounded-lg mt-10">
-          <div className="text-2xl text-[#626262] dark:text-[#E2E2E2] my-5 mx-auto w-fit h-fit">
-            Organizations
-          </div>
-          <div className="overflow-y-scroll h-[470px]">
-            {organizationData &&
-              organizationData.map((value, index) => (
-                <div
-                  key={index}
-                  className="flex justify-between items-center w-[400px] h-[80px] mx-auto my-2 border-b-[1px] border-[#DDDDDD]"
-                >
-                  <div className="flex justify-between items-center">
-                    <div className="ml-2 text-[#7E7E7E]">{index + 1}</div>
-                    <div className="w-10 h-10 bg-[#498ade] mx-4 rounded-full"></div>
-                    <a href={`/user/${value.handle}`} target="_blank">
-                      <div
-                        className={`${
-                          index == 0
-                            ? 'text-[#FFD600]'
-                            : index == 1
-                            ? 'text-[#CCCCCC]'
-                            : index == 2
-                            ? 'text-[#8E5E00]'
-                            : 'text-[#A3A3A3]'
-                        } `}
-                      >
-                        {value.handle}
-                      </div>
-                    </a>
-                  </div>
-                  <div className="text-[#A3A3A3] text-sm">
-                    {value.amount} VHRs
-                  </div>
-                </div>
-              ))}
-            {organizationsIsLoading && (
-              <div className="flex justify-between items-center w-[400px] h-[80px] mx-auto my-2 border-[#DDDDDD]">
-                <Spinner />
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
+        </GridItemTwelve>
+      </GridLayout>
+    </>
   )
 }
 
