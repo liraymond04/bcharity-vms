1313721f5fcc0ea704e42f9a0e126b12a7cd4d35
liraymond04 <li.raymond04@gmail.com>
2023-08-04 12:30:54 -0600

    Add data fetching to VHR leaderboard

--- apps/app/src/components/VHRs/index.tsx
+++ apps/app/src/components/VHRs/index.tsx
@@ -1,47 +1,99 @@
 import { Inter } from '@next/font/google'
+import JSSoup from 'jssoup'
 import { NextPage } from 'next'
-import { useEffect, useState } from 'react'
+import { SetStateAction, useEffect, useState } from 'react'
+
+import { VHR_TOP_HOLDERS_URL } from '@/constants'
+import isVerified from '@/lib/isVerified'
+import getProfilesOwnedBy from '@/lib/lens-protocol/getProfilesOwnedBy'
+
+import { Spinner } from '../UI/Spinner'
+
+interface Item {
+  index: number
+  address: string
+  handle: string
+  amount: number
+  percentage: string
+  org: boolean
+}
 
 const inter500 = Inter({
   subsets: ['latin'],
   weight: ['500']
 })
 
-const generateVolunteerData = () => {
-  var data = []
-  var last = 1e9
-  for (let i = 0; i < 20; i++) {
-    const op = {
-      name: 'Volunteer ' + (i + 1),
-      vhr: Math.floor(Math.random() * last)
-    }
-    last = op.vhr
-    data.push(op)
-  }
-  return data
-}
-
-const generateOrganizationData = () => {
-  var data = []
-  var last = 1e9
-  for (let i = 0; i < 20; i++) {
-    const op = {
-      name: 'Organization ' + (i + 1),
-      vhr: Math.floor(Math.random() * last)
-    }
-    last = op.vhr
-    data.push(op)
-  }
-  return data
-}
-
 const VHRs: NextPage = () => {
-  const [volunteerData, setVolunteerData] = useState<any[]>()
-  const [organizationData, setOrganizationData] = useState<any[]>()
+  const [volunteerData, setVolunteerData] = useState<Item[]>()
+  const [volunteersIsLoading, setVolunteersIsLoading] = useState<boolean>(false)
+
+  const [organizationData, setOrganizationData] = useState<Item[]>()
+  const [organizationsIsLoading, setOrganizationsIsLoading] =
+    useState<boolean>(false)
+
+  const sortItems = async (
+    items: Item[],
+    setData: (value: SetStateAction<Item[] | undefined>) => void,
+    setIsLoading: (value: SetStateAction<boolean>) => void,
+    verified: boolean
+  ) => {
+    let arr: Item[] = []
+    for (let i = 0; i < items.length; i++) {
+      let item = Object.create(items[i])
+      let profiles = await getProfilesOwnedBy(item.address)
+      profiles = profiles.filter(
+        (profile) => isVerified(profile.id) === verified
+      )
+      item.handle = profiles[0]?.handle
+      if (profiles.length > 0) {
+        arr.push(item)
+        setData(arr)
+      }
+    }
+    setIsLoading(false)
+  }
+
+  const getHolders = async () => {
+    setVolunteersIsLoading(true)
+    setOrganizationsIsLoading(true)
+    try {
+      const response = await fetch(`api/cors?url=${VHR_TOP_HOLDERS_URL}`)
+      const html = await response.text()
+
+      // scraping
+      const soup = new JSSoup(html)
+      const tag = soup.findAll('td')
+      let index = 0
+      let items: Item[] = []
+      let cur = []
+      for (let i = 0; i < tag.length; i++) {
+        cur[i % 4] = tag[i].text
+        if (i % 4 === 0 && i !== 0) {
+          items[index] = {
+            index: index,
+            address: cur[1],
+            handle: '',
+            amount: Number(cur[2]?.replace(/,/g, '')),
+            percentage: cur[3],
+            org: false
+          }
+          index++
+        }
+      }
+
+      sortItems(items, setVolunteerData, setVolunteersIsLoading, false)
+      sortItems(items, setOrganizationData, setOrganizationsIsLoading, true)
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log(e)
+      }
+      setVolunteersIsLoading(false)
+      setOrganizationsIsLoading(false)
+    }
+  }
 
   useEffect(() => {
-    setVolunteerData(generateVolunteerData())
-    setOrganizationData(generateOrganizationData())
+    getHolders()
   }, [])
 
   return (
@@ -65,8 +117,8 @@ const VHRs: NextPage = () => {
                 >
                   <div className="flex justify-between items-center">
                     <div className="ml-2 text-[#7E7E7E]">{index + 1}</div>
-                    <div className="w-10 h-10 bg-[#EB7575] mx-4 rounded-full"></div>
-                    <a href={`/user/${value.name}`}>
+                    <div className="w-10 h-10 bg-[#498ade] mx-4 rounded-full"></div>
+                    <a href={`/user/${value.handle}`} target="_blank">
                       <div
                         className={`${
                           index == 0
@@ -76,15 +128,22 @@ const VHRs: NextPage = () => {
                             : index == 2
                             ? 'text-[#8E5E00]'
                             : 'text-[#A3A3A3]'
-                        }`}
+                        } `}
                       >
-                        {value.name}
+                        {value.handle}
                       </div>
                     </a>
                   </div>
-                  <div className="text-[#A3A3A3] text-sm">{value.vhr} VHRs</div>
+                  <div className="text-[#A3A3A3] text-sm">
+                    {value.amount} VHRs
+                  </div>
                 </div>
               ))}
+            {volunteersIsLoading && (
+              <div className="flex justify-between items-center w-[400px] h-[80px] mx-auto my-2 border-[#DDDDDD]">
+                <Spinner />
+              </div>
+            )}
           </div>
         </div>
         <div className="min-w-[450px] mx-2 h-[550px] bg-[#F9F9F9] dark:bg-[#18004A] rounded-lg mt-10">
@@ -101,7 +160,7 @@ const VHRs: NextPage = () => {
                   <div className="flex justify-between items-center">
                     <div className="ml-2 text-[#7E7E7E]">{index + 1}</div>
                     <div className="w-10 h-10 bg-[#498ade] mx-4 rounded-full"></div>
-                    <a href={`/user/${value.name}`}>
+                    <a href={`/user/${value.handle}`} target="_blank">
                       <div
                         className={`${
                           index == 0
@@ -113,13 +172,20 @@ const VHRs: NextPage = () => {
                             : 'text-[#A3A3A3]'
                         } `}
                       >
-                        {value.name}
+                        {value.handle}
                       </div>
                     </a>
                   </div>
-                  <div className="text-[#A3A3A3] text-sm">{value.vhr} VHRs</div>
+                  <div className="text-[#A3A3A3] text-sm">
+                    {value.amount} VHRs
+                  </div>
                 </div>
               ))}
+            {organizationsIsLoading && (
+              <div className="flex justify-between items-center w-[400px] h-[80px] mx-auto my-2 border-[#DDDDDD]">
+                <Spinner />
+              </div>
+            )}
           </div>
         </div>
       </div>
