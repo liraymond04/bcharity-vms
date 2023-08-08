29599057a94e4c4c3b63e16937cc663eccf39023
Ezra Huang <ezrahuang155@gmail.com>
2023-08-08 12:08:22 -0600

    hookify createPost and createComment

---
+++ apps/app/src/lib/lens-protocol/useCreateComment.ts
@@ -0,0 +1,55 @@
+import {
+  CollectModuleParams,
+  PublicationMetadataV2Input,
+  ReferenceModuleParams
+} from '@lens-protocol/client'
+import { ProfileFragment as Profile } from '@lens-protocol/client'
+import { useStorageUpload } from '@thirdweb-dev/react'
+import { signTypedData } from '@wagmi/core'
+
+import getSignature from './getSignature'
+import lensClient from './lensClient'
+
+const useCreateComment = (
+  publicationId: string,
+  profile: Profile,
+  metadata: PublicationMetadataV2Input,
+  collectModule: CollectModuleParams = {
+    freeCollectModule: { followerOnly: false }
+  },
+  referenceModule: ReferenceModuleParams = {
+    followerOnlyReferenceModule: false
+  }
+) => {
+  const { mutateAsync: upload } = useStorageUpload()
+
+  const createComment = async () => {
+    const contentURI = (await upload({ data: [metadata] }))[0]
+
+    const profileId: string = profile.id
+
+    const typedDataResult =
+      await lensClient().publication.createCommentTypedData({
+        publicationId,
+        profileId,
+        contentURI,
+        collectModule,
+        referenceModule
+      })
+
+    const signature = await signTypedData(
+      getSignature(typedDataResult.unwrap().typedData)
+    )
+
+    const broadcastResult = await lensClient().transaction.broadcast({
+      id: typedDataResult.unwrap().id,
+      signature: signature
+    })
+
+    return broadcastResult
+  }
+
+  return { createComment }
+}
+
+export default useCreateComment
