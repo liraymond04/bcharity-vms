f2ec8696659b6a1a5eec4b0c5e7ad67961c1b231
liraymond04 <li.raymond04@gmail.com>
2023-08-08 12:49:34 -0600

    Update locale for inlang mt and nested keys

--- apps/app/src/pages/404.tsx
+++ apps/app/src/pages/404.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next'
 import { STATIC_ASSETS } from 'src/constants'
 
 export default function Custom404() {
-  const { t } = useTranslation('common')
+  const { t } = useTranslation('common', { keyPrefix: '404' })
   return (
     <div className="flex-col page-center">
       <SEO title="404 • BCharity VMS" />
@@ -18,15 +18,15 @@ export default function Custom404() {
         height={240}
       />
       <div className="py-10 text-center">
-        <h1 className="mb-4 text-3xl font-bold">{t('Oops, Lost?')}</h1>
-        <div className="mb-4">{t('This page could not be found.')}</div>
+        <h1 className="mb-4 text-3xl font-bold">{t('lost')}</h1>
+        <div className="mb-4">{t('not-found')}</div>
         <Link href="/">
           <Button
             className="flex mx-auto item-center"
             size="lg"
             icon={<HomeIcon className="w-4 h-4" />}
           >
-            <div>{t('Go Home')}</div>
+            <div>{t('go-home')}</div>
           </Button>
         </Link>
       </div>
