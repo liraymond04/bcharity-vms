81837ab2625d93f3cc234e7468b7476aeba6fda9
Lithium Engineer <62958090+LithiumEngineer@users.noreply.github.com>
2023-08-10 15:20:28 -0600

    popup effect on hover

--- apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerLogHours.tsx
+++ apps/app/src/components/Dashboard/VolunteerDashboard/VolunteerLogHours.tsx
@@ -9,6 +9,7 @@ import { PostFragment, PublicationTypes } from '@lens-protocol/client'
 import { Inter } from '@next/font/google'
 import React, { useEffect, useState } from 'react'
 
+import ClearFilters from '@/components/Shared/ClearFilters'
 import { Spinner } from '@/components/UI/Spinner'
 import usePostData from '@/lib/lens-protocol/usePostData'
 import {
@@ -117,17 +118,11 @@ const VolunteerLogHours: React.FC<IVolunteerLogHoursProps> = () => {
             onClick={(c) => setSelectedCategory(c)}
           />
         </div>
-        <button
-          className="ml-3 min-w-[110px] h-fit text-red-500 bg-[#ffc2d4] border-red-500 border-2 rounded-md px-2 hover:bg-red-500 hover:text-white hover:cursor-pointer dark:text-[#fff2f2] dark:text-opacity-80 dark:bg-[#555591] dark:border-[#20203f] dark:hover:bg-[#20203f] dark:hover:text-opacity-100 dark:hover:text-[#ff8585]"
+        <ClearFilters
           onClick={() => {
-            setSelectedSortBy('')
             setSelectedCategory('')
-            setIndice(resetIndice())
-            setDisplayIndex(-1)
           }}
-        >
-          Clear Filters
-        </button>
+        />
       </div>
 
       <div
@@ -147,7 +142,7 @@ const VolunteerLogHours: React.FC<IVolunteerLogHoursProps> = () => {
               )
               .map((op) => (
                 <div
-                  className={`flex justify-between items-center my-5 tracking-wide w-[800px] h-[50px] bg-[#CEBBF8] bg-opacity-[0.50] rounded-md shadow-md hover:bg-opacity-100 hover:cursor-pointer hover:h-[60px] duration-100 ${
+                  className={`flex justify-between items-center my-5 tracking-wide w-[800px] h-[50px] bg-[#CEBBF8] bg-opacity-[0.50] rounded-md shadow-md hover:bg-opacity-100 hover:cursor-pointer hover:scale-y-110 duration-100 ${
                     inter500.className
                   } ${displayIndex == op ? 'bg-blue-200' : ''}`}
                   key={op}
