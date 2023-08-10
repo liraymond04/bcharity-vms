6f5fb0ac811e157dc18473c0e64de7caac573680
liraymond04 <li.raymond04@gmail.com>
2023-08-10 15:32:12 -0600

    Add types to clear filters component

--- apps/app/src/components/Shared/ClearFilters.tsx
+++ apps/app/src/components/Shared/ClearFilters.tsx
@@ -1,4 +1,10 @@
-const ClearFilters = ({ onClick }) => {
+import { FC } from 'react'
+
+interface Props {
+  onClick: Function
+}
+
+const ClearFilters: FC<Props> = ({ onClick }) => {
   return (
     <button
       className="ml-3 min-w-[110px] h-fit text-red-500 bg-[#ffc2d4] border-red-500 border-2 rounded-md px-2 hover:bg-red-500 hover:cursor-pointer dark:text-[#fff2f2] dark:text-opacity-80 dark:bg-[#555591] dark:border-[#20203f] dark:hover:text-opacity-100 dark:hover:text-[#ff8585]"
