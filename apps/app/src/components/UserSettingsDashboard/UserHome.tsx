9692493ecf2b155dfceb45050461f9aef34cf128
liraymond04 <li.raymond04@gmail.com>
2023-08-10 10:52:17 -0600

    Fix user settings update profile metadata form

--- apps/app/src/components/UserSettingsDashboard/UserHome.tsx
+++ apps/app/src/components/UserSettingsDashboard/UserHome.tsx
@@ -5,7 +5,6 @@ import { v4 } from 'uuid'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
 import { Input } from '@/components/UI/Input'
-import { Spinner } from '@/components/UI/Spinner'
 import { TextArea } from '@/components/UI/TextArea'
 import checkAuth from '@/lib/lens-protocol/checkAuth'
 import getSignature from '@/lib/lens-protocol/getSignature'
@@ -20,6 +19,7 @@ import { useAppPersistStore } from '@/store/app'
 
 import { Button } from '../UI/Button'
 import { Card } from '../UI/Card'
+import { Spinner } from '../UI/Spinner'
 import SelectAvatar from './UserHome/SelectAvatar'
 
 const VolunteerHomeTab: React.FC = () => {
@@ -68,65 +68,65 @@ const VolunteerHomeTab: React.FC = () => {
   }, [currentUser])
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault
+    event.preventDefault()
     setIsLoading(true)
 
     try {
       if (currentUser) {
-        checkAuth(currentUser?.ownedBy).then(async () => {
-          const attributes: AttributeData[] = [
-            {
-              displayType: MetadataDisplayType.string,
-              traitType: 'website',
-              value: website,
-              key: 'website'
-            },
-            {
-              displayType: MetadataDisplayType.string,
-              traitType: 'location',
-              value: location,
-              key: 'location'
-            }
-          ]
+        await checkAuth(currentUser?.ownedBy)
 
-          const avatarUrl = cover ? (await upload({ data: [cover] }))[0] : null
-
-          const metadata: ProfileMetadata = {
-            version: MetadataVersion.ProfileMetadataVersions['1.0.0'],
-            metadata_id: v4(),
-            name,
-            bio,
-            cover_picture: avatarUrl,
-            attributes
+        const attributes: AttributeData[] = [
+          {
+            displayType: MetadataDisplayType.string,
+            traitType: 'website',
+            value: website,
+            key: 'website'
+          },
+          {
+            displayType: MetadataDisplayType.string,
+            traitType: 'location',
+            value: location,
+            key: 'location'
           }
+        ]
 
-          const metadataUrl = sdk?.storage.resolveScheme(
-            (await upload({ data: [metadata] }))[0]
-          )
+        const avatarUrl = cover ? (await upload({ data: [cover] }))[0] : null
 
-          if (!metadataUrl) throw Error('Metadata upload failed')
+        const metadata: ProfileMetadata = {
+          version: MetadataVersion.ProfileMetadataVersions['1.0.0'],
+          metadata_id: v4(),
+          name,
+          bio,
+          cover_picture: avatarUrl,
+          attributes
+        }
 
-          const typedDataResult =
-            await lensClient().profile.createSetProfileMetadataTypedData({
-              metadata: metadataUrl,
-              profileId: currentUser?.id
-            })
+        const metadataUrl = sdk?.storage.resolveScheme(
+          (await upload({ data: [metadata] }))[0]
+        )
 
-          const signature = await signTypedData(
-            getSignature(typedDataResult.unwrap().typedData)
-          )
+        if (!metadataUrl) throw Error('Metadata upload failed')
 
-          const broadcastResult = await lensClient().transaction.broadcast({
-            id: typedDataResult.unwrap().id,
-            signature: signature
+        const typedDataResult =
+          await lensClient().profile.createSetProfileMetadataTypedData({
+            metadata: metadataUrl,
+            profileId: currentUser?.id
           })
 
-          setIsLoading(false)
+        const signature = await signTypedData(
+          getSignature(typedDataResult.unwrap().typedData)
+        )
+
+        const broadcastResult = await lensClient().transaction.broadcast({
+          id: typedDataResult.unwrap().id,
+          signature: signature
         })
       }
       console.log('Profile saved successfully')
     } catch (error) {
       console.error('Error updating profile:', error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -134,71 +134,72 @@ const VolunteerHomeTab: React.FC = () => {
     <GridLayout>
       <GridItemTwelve>
         <Card>
-          {isLoading ? (
-            <Spinner />
-          ) : (
-            <form className="my-5 mx-5" onSubmit={handleSubmit}>
-              <div>
-                <Input
-                  label="Profile ID: "
-                  type="text"
-                  id="profileId"
-                  className="text-gray-600 dark:text-gray-500"
-                  value={currentUser?.id}
-                  readOnly
-                />
-              </div>
-              <div>
-                <Input
-                  label="Name: "
-                  type="text"
-                  id="name"
-                  value={currentUser?.handle}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div>
-                <Input
-                  label="Location: "
-                  type="text"
-                  id="location"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                />
-              </div>
-              <div>
-                <TextArea
-                  label="Bio: "
-                  id="bio"
-                  value={bio}
-                  onChange={(e) => setBio(e.target.value)}
-                  rows={5}
-                />
-              </div>
-              <div>
-                <Input
-                  label="Website: "
-                  type="text"
-                  id="website"
-                  value={website}
-                  onChange={(e) => setWebsite(e.target.value)}
-                />
-              </div>
-              <div>
-                <Input
-                  label="Cover: "
-                  type="file"
-                  id="cover"
-                  onChange={(e) => setCover(e.target.files?.[0] || null)}
-                />
-              </div>
-              <div className="flex justify-end">
-                <Button className="my-5" type="submit">
-                  Save
-                </Button>
-              </div>
-            </form>
-          )}
+          <form className="my-5 mx-5" onSubmit={handleSubmit}>
+            <div>
+              <Input
+                label="Profile ID: "
+                type="text"
+                id="profileId"
+                className="text-gray-600 dark:text-gray-500"
+                value={currentUser?.id}
+                readOnly
+              />
+            </div>
+            <div>
+              <Input
+                label="Name: "
+                type="text"
+                id="name"
+                value={currentUser?.handle}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <Input
+                label="Location: "
+                type="text"
+                id="location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </div>
+            <div>
+              <TextArea
+                label="Bio: "
+                id="bio"
+                value={bio}
+                onChange={(e) => setBio(e.target.value)}
+                rows={5}
+              />
+            </div>
+            <div>
+              <Input
+                label="Website: "
+                type="text"
+                id="website"
+                value={website}
+                onChange={(e) => setWebsite(e.target.value)}
+              />
+            </div>
+            <div>
+              <Input
+                label="Cover: "
+                type="file"
+                id="cover"
+                onChange={(e) => setCover(e.target.files?.[0] || null)}
+              />
+            </div>
+            <div className="flex justify-end">
+              <Button
+                className="my-5"
+                disabled={isLoading}
+                icon={isLoading && <Spinner size="sm" />}
+                type="submit"
+              >
+                Save
+              </Button>
+            </div>
+          </form>
         </Card>
       </GridItemTwelve>
       <GridItemTwelve>
