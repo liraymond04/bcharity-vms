3937e66cae87d89a4d34035907f7b8828651e2f7
Ezra Huang <ezrahuang155@gmail.com>
2023-08-14 11:31:14 -0600

    docs for checkAuth

--- apps/app/src/lib/lens-protocol/checkAuth.ts
+++ apps/app/src/lib/lens-protocol/checkAuth.ts
@@ -2,6 +2,23 @@ import { signMessage } from '@wagmi/core'
 
 import lensClient from './lensClient'
 
+/**
+ * A function to check authentication usually used before continuing with another
+ * lens client query that requires authentication.
+ *
+ * See {@link https://docs.lens.xyz/docs/authentication#using-lensclient-sdk} for lens-related documentation.
+ *
+ * @param address the Ethereum address of the profile that is requesting authentication
+ * (usually obtained from ProfileFragment.ownedBy)
+ *
+ * @example Check authentication before creating a post
+ * ```ts
+ * const publisher: ProfileFragment = ...
+ * const metadata = ...
+ * await checkAuth(publisher.ownedBy)
+ * const createPostResult = await createPost(publisher, metadata)
+ * ```
+ */
 const checkAuth = async (address: string) => {
   const authenticated = await lensClient().authentication.isAuthenticated()
   if (!authenticated) {
