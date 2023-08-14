bf8356d7124c6ede3d1bf0437fae0521692b9649
Ezra Huang <ezrahuang155@gmail.com>
2023-08-14 11:18:38 -0600

    setup docs

---
+++ apps/app/src/lib/index.ts
@@ -0,0 +1,4 @@
+import * as lens from './lens-protocol'
+import * as metadata from './metadata'
+
+export { lens, metadata }
---
+++ apps/app/src/lib/lens-protocol/index.ts
@@ -0,0 +1,39 @@
+import checkAuth from './checkAuth'
+import createCollect from './createCollect'
+import createProfile from './createProfile'
+import { formatLocation } from './formatLocation'
+import getProfile from './getProfile'
+import getProfilesOwnedBy from './getProfilesOwnedBy'
+import getSignature from './getSignature'
+import lensClient from './lensClient'
+import useApply from './useApply'
+import useBookmark from './useBookmark'
+import useCreateComment from './useCreateComment'
+import useCreatePost from './useCreatePost'
+import useEnabledCurrencies from './useEnabledCurrencies'
+import useExplorePublications from './useExplorePublications'
+import useFollow from './useFollow'
+import useLazyGeolocation from './useLazyGeolocation'
+import usePostData from './usePostData'
+import usePublication from './usePublication'
+
+export {
+  checkAuth,
+  createCollect,
+  createProfile,
+  formatLocation,
+  getProfile,
+  getProfilesOwnedBy,
+  getSignature,
+  lensClient,
+  useApply,
+  useBookmark,
+  useCreateComment,
+  useCreatePost,
+  useEnabledCurrencies,
+  useExplorePublications,
+  useFollow,
+  useLazyGeolocation,
+  usePostData,
+  usePublication
+}
