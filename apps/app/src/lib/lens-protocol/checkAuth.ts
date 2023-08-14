db1c796f3aaf0a44d09d3112537768dce78d8ee1
liraymond04 <li.raymond04@gmail.com>
2023-08-14 11:29:05 -0600

    Force logout if connector error occurs

--- apps/app/src/lib/lens-protocol/checkAuth.ts
+++ apps/app/src/lib/lens-protocol/checkAuth.ts
@@ -1,16 +1,36 @@
 import { signMessage } from '@wagmi/core'
+import router from 'next/router'
+
+import { resetCurrentUser } from '@/store/app'
 
 import lensClient from './lensClient'
 
-const checkAuth = async (address: string) => {
-  const authenticated = await lensClient().authentication.isAuthenticated()
-  if (!authenticated) {
-    const challenge = await lensClient().authentication.generateChallenge(
-      address
-    )
-    const signature = await signMessage({ message: challenge })
+const logout = () => {
+  if (router.pathname === '/dashboard' || router.pathname === '/settings') {
+    router.push('/')
+  }
+  resetCurrentUser()
+  localStorage.removeItem('bcharity.store')
+}
 
-    await lensClient().authentication.authenticate(address, signature)
+const checkAuth = async (address: string) => {
+  try {
+    const authenticated = await lensClient().authentication.isAuthenticated()
+    if (!authenticated) {
+      const challenge = await lensClient().authentication.generateChallenge(
+        address
+      )
+      const signature = await signMessage({ message: challenge })
+
+      await lensClient().authentication.authenticate(address, signature)
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      if (e.name === 'ConnectorNotFoundError') {
+        logout()
+      }
+      throw e
+    }
   }
 }
 
