9279c3328f0823a0c7262163a17bc685332ccf14
liraymond04 <li.raymond04@gmail.com>
2023-08-15 15:32:42 -0600

    Add apply to opportunity modal translation

--- apps/app/src/components/Volunteers/ApplyToOpportunityModal.tsx
+++ apps/app/src/components/Volunteers/ApplyToOpportunityModal.tsx
@@ -3,6 +3,7 @@ import { Modal } from '@components/UI/Modal'
 import Link from 'next/link'
 import { FC, useState } from 'react'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 export const NextLink = ({ href, children, ...rest }: Record<string, any>) => (
   <Link href={href} {...rest}>
@@ -11,6 +12,9 @@ export const NextLink = ({ href, children, ...rest }: Record<string, any>) => (
 )
 
 const ApplyToOpportunityModal: FC = () => {
+  const { t } = useTranslation('common', {
+    keyPrefix: 'components.volunteers.apply-to-opportunity-modal'
+  })
   const [showModal, setShowModal] = useState<boolean>(false)
 
   function setImage(arg0: File | null): void {
@@ -26,11 +30,16 @@ const ApplyToOpportunityModal: FC = () => {
     >
       <div className="pl-10">
         <div className="flex flex-row ">
-          <div className="text-purple-500 text-5xl font-bold">
-            Apply to Opportunity
+          <div
+            className="text-purple-500 text-5xl font-bold"
+            suppressHydrationWarning
+          >
+            {t('title')}
           </div>
         </div>
-        <div className="text-3xl font-semibold mt-10">Upload Resume</div>
+        <div className="text-3xl font-semibold mt-10" suppressHydrationWarning>
+          {t('upload')}
+        </div>
         <div className="flex items-center justify-left w-full mt-8 ">
           <label
             htmlFor="dropzone-file"
@@ -52,9 +61,14 @@ const ApplyToOpportunityModal: FC = () => {
                   d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                 />
               </svg>
-              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                <span className="font-semibold">Click to upload</span> or drag
-                and drop
+              <p
+                className="mb-2 text-sm text-gray-500 dark:text-gray-400"
+                suppressHydrationWarning
+              >
+                <span className="font-semibold" suppressHydrationWarning>
+                  {t('click-to-upload')}
+                </span>{' '}
+                {t('drag-and-drop')}
               </p>
               <p className="text-xs text-gray-500 dark:text-gray-400">PDF </p>
             </div>
@@ -65,16 +79,20 @@ const ApplyToOpportunityModal: FC = () => {
           <label
             htmlFor="large-input"
             className="block mb-2 text-3xl font-semibold mt-8 text-gray-900 dark:text-white"
+            suppressHydrationWarning
           >
-            Description
+            {t('description')}
           </label>
           <textarea className="resize-y w-11/12 rounded-md dark:text-black"></textarea>
         </div>
       </div>
       <div className="flex items-center justify-center">
         {' '}
-        <Button className=" shrink-0 text-2xl  mt-8 mb-12 w-52 h-16  align-middle">
-          Submit
+        <Button
+          className=" shrink-0 text-2xl  mt-8 mb-12 w-52 h-16  align-middle"
+          suppressHydrationWarning
+        >
+          {t('submit')}
         </Button>
       </div>
     </Modal>
