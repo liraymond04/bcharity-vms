82a6d04a80120b8407126fefa644ad5ebc4b0624
Ezra Huang <ezrahuang155@gmail.com>
2023-08-15 15:06:45 -0600

    prevent horizonal overflow from causing header clip in dashboard

--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationDashboard.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationDashboard.tsx
@@ -54,7 +54,7 @@ const OrganizationDashboard: React.FC = () => {
   ]
 
   return (
-    <div className="flex">
+    <div className="flex max-w-screen overflow-scroll">
       <Sidebar
         emptyTop
         selectedIndex={selectedTab}
