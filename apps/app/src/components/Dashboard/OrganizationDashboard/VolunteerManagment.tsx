a0db024574bffe056550cf49f5b5b81cc0850114
liraymond04 <li.raymond04@gmail.com>
2023-08-21 17:39:17 -0600

    Fix build errors Move test data to array and map components

--- apps/app/src/components/Dashboard/OrganizationDashboard/VolunteerManagment.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/VolunteerManagment.tsx
@@ -1,5 +1,5 @@
 import { PlusCircleIcon, SearchIcon } from '@heroicons/react/outline'
-import React, { useState } from 'react'
+import React, { MouseEventHandler, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
@@ -14,28 +14,36 @@ interface VolunteerInfoProps {
   bio: string
   location: string
   date: string
-  avatar: string
+  avatar?: string
 }
+
 interface PurpleBoxProps {
   selected?: boolean
   userName: string
   dateCreated: string
+  onClick?: MouseEventHandler<HTMLDivElement>
 }
 
-const PurpleBox: React.FC<PurpleBoxProps> = (lala) => {
-  const boxClassName = lala.selected
+const PurpleBox: React.FC<PurpleBoxProps> = ({
+  selected,
+  userName,
+  dateCreated,
+  onClick
+}) => {
+  const boxClassName = selected
     ? 'bg-blue-100 dark:bg-violet-500'
     : 'bg-violet-200 dark:bg-Within dark:bg-opacity-10'
   return (
     <div
       className={`items-center shadow-sm shadow-black px-5 mx-5 my-3 h-16 py-2 ${boxClassName}`}
+      onClick={onClick}
     >
-      <div>{lala.userName}</div>
+      <div>{userName}</div>
       <div className="flex justify-end my-3">
         <div className="text-sm font-extralight">
           Application date created:&nbsp;
         </div>
-        <div className="text-sm font-extralight">{lala.dateCreated}</div>
+        <div className="text-sm font-extralight">{dateCreated}</div>
       </div>
     </div>
   )
@@ -57,12 +65,14 @@ const VolunteerInfoCard: React.FC<VolunteerInfoProps> = (info) => {
         <p> wants to work with your organization</p>
       </div>
       <div className="flex">
-        <img
-          className="rounded-sm py-3"
-          src={info.avatar}
-          alt="Rounded avatar"
-          style={{ width: '100px', height: 'auto' }}
-        />
+        {info.avatar !== undefined && (
+          <img
+            className="rounded-sm py-3"
+            src={info.avatar}
+            alt="Rounded avatar"
+            style={{ width: '100px', height: 'auto' }}
+          />
+        )}
         <div className="flex justify-between py-3 pl-5">
           <div className="text-violet-500">bio:&nbsp;</div>
           <p>{info.bio}</p>
@@ -88,10 +98,91 @@ const VolunteerInfoCard: React.FC<VolunteerInfoProps> = (info) => {
     </Card>
   )
 }
-const VolunteerManagmentTab: React.FC<PurpleBoxProps> = ({ selected }) => {
+
+const VolunteerManagmentTab: React.FC = () => {
   const [searchValue, setSearchValue] = useState('')
   const [categories] = useState<Set<string>>(new Set())
   const [selectedCategory, setSelectedCategory] = useState<string>('')
+  const [selected, setSelected] = useState<number>()
+
+  const testData = [
+    {
+      userName: 'May',
+      dateCreated: '6969',
+      location: 'france',
+      bio: 'im weird',
+      avatar:
+        'https://www.boredpanda.com/blog/wp-content/uploads/2021/06/Meet-Gertrude-the-duck-with-more-stylish-hair-than-yours-60d992b564567__700.jpg'
+    },
+    {
+      userName: 'bLAH',
+      dateCreated: '6969',
+      location: 'uk',
+      bio: 'bio 1',
+      avatar:
+        'https://blog.udemy.com/wp-content/uploads/2014/05/bigstock-test-icon-63758263.jpg'
+    },
+    {
+      userName: 'aksmc',
+      dateCreated: '6969',
+      location: 'canada',
+      bio: 'bio 2'
+    },
+    {
+      userName: 'May',
+      dateCreated: '6969',
+      location: 'usa',
+      bio: 'bio 3'
+    },
+    {
+      userName: 'lali',
+      dateCreated: '6969',
+      location: 'germany',
+      bio: 'bio 4'
+    },
+    {
+      userName: 'Michael',
+      dateCreated: '6969',
+      location: 'china',
+      bio: 'bio 5'
+    },
+    {
+      userName: 'johnny',
+      dateCreated: '6969',
+      location: 'korea',
+      bio: 'bio 6'
+    },
+    {
+      userName: 'lalalalaal',
+      dateCreated: '6969',
+      location: 'poland',
+      bio: 'bio 7'
+    },
+    {
+      userName: 'say less',
+      dateCreated: '6969',
+      location: 'mexico',
+      bio: 'bio 8'
+    },
+    {
+      userName: 'stop this',
+      dateCreated: '6969',
+      location: 'indonesia',
+      bio: 'bio 9'
+    },
+    {
+      userName: 'help',
+      dateCreated: '6969',
+      location: 'spain',
+      bio: 'bio 10'
+    },
+    {
+      userName: 'sry',
+      dateCreated: '6969',
+      location: 'egypt',
+      bio: 'bio 11'
+    }
+  ]
 
   return (
     <GridLayout>
@@ -143,78 +234,35 @@ const VolunteerManagmentTab: React.FC<PurpleBoxProps> = ({ selected }) => {
         <div className="flex space-x-24 pt-5">
           <Card className={`h-1/2 w-1/2`}>
             <div className="scrollbar">
-              <PurpleBox
-                selected={selected}
-                userName="May"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="blah"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="yay"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="amksmc"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="lali"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="Michael"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="johnny"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="lalalalaal"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="say less"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="stop this"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="help"
-                dateCreated="6969"
-              />
-              <PurpleBox
-                selected={selected}
-                userName="sry"
-                dateCreated="6969"
-              />
+              {testData.map((item, i) => {
+                return (
+                  <PurpleBox
+                    key={i}
+                    selected={selected === i}
+                    userName={item.userName}
+                    dateCreated={item.dateCreated}
+                    onClick={() => {
+                      if (selected === i) setSelected(undefined)
+                      else setSelected(i)
+                    }}
+                  />
+                )
+              })}
+
               {/* the box placeholder for the data ^ */}
             </div>
           </Card>
-          <div className="pb-10">
-            <VolunteerInfoCard
-              volName="may"
-              location="france"
-              date="6969"
-              bio="im weird"
-              avatar="https://www.boredpanda.com/blog/wp-content/uploads/2021/06/Meet-Gertrude-the-duck-with-more-stylish-hair-than-yours-60d992b564567__700.jpg"
-            />
-          </div>
+          {selected !== undefined && (
+            <div className="pb-10">
+              <VolunteerInfoCard
+                volName={testData[selected].userName}
+                location={testData[selected].location}
+                date={testData[selected].dateCreated}
+                bio={testData[selected].bio}
+                avatar={testData[selected].avatar}
+              />
+            </div>
+          )}
         </div>
       </GridItemTwelve>
     </GridLayout>
