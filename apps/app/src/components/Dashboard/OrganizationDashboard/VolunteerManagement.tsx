76c3c302f942800cb3a9629d0079cd65ecf97a9f
Ezra Huang <ezrahuang155@gmail.com>
2023-08-21 20:32:55 -0400

    integrate data fetch hook for applications

--- apps/app/src/components/Dashboard/OrganizationDashboard/VolunteerManagement.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/VolunteerManagement.tsx
@@ -1,20 +1,22 @@
 import { PlusCircleIcon, SearchIcon } from '@heroicons/react/outline'
-import React, { MouseEventHandler, useState } from 'react'
+import { ProfileFragment } from '@lens-protocol/client'
+import React, { MouseEventHandler, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
 import ClearFilters from '@/components/Shared/ClearFilters'
 import { Button } from '@/components/UI/Button'
 import { Card } from '@/components/UI/Card'
+import { Spinner } from '@/components/UI/Spinner'
+import { getAvatar, lensClient } from '@/lib/lens-protocol'
+import useApplications from '@/lib/lens-protocol/useApplications'
+import { ApplicationMetadata } from '@/lib/metadata'
+import { useAppPersistStore } from '@/store/app'
 
 import DashboardDropDown from '../VolunteerDashboard/DashboardDropDown'
 
 interface VolunteerInfoProps {
-  volName: string
-  bio: string
-  location: string
-  date: string
-  avatar?: string
+  application: ApplicationMetadata
 }
 
 interface PurpleBoxProps {
@@ -49,10 +51,40 @@ const PurpleBox: React.FC<PurpleBoxProps> = ({
   )
 }
 
-const VolunteerInfoCard: React.FC<VolunteerInfoProps> = (info) => {
+const VolunteerInfoCard: React.FC<VolunteerInfoProps> = ({ application }) => {
   const { t } = useTranslation('common', {
     keyPrefix: 'components.dashboard.organization.volunteer-managment'
   })
+
+  const { t: e } = useTranslation('common', { keyPrefix: 'errors' })
+
+  const [userProfile, setUserProfile] = useState<ProfileFragment>()
+
+  const [profileDataLoading, setProfileDataLoading] = useState(false)
+  const [profileError, setProfileError] = useState('')
+
+  useEffect(() => {
+    setProfileDataLoading(true)
+
+    lensClient()
+      .profile.fetch({
+        profileId: application.from.id
+      })
+      .then((p) => {
+        if (p) {
+          setUserProfile(p)
+        } else {
+          setProfileError(e('profile-null'))
+        }
+      })
+      .finally(() => setProfileDataLoading(false))
+  }, [application.from.id])
+
+  const location = useMemo(() => {
+    return userProfile?.attributes?.find((attr) => attr.key === 'location')
+      ?.value
+  }, [userProfile])
+
   return (
     <Card className="pt-10 pl-10 pr-10 justify-center">
       <div className="justify-center font-black text-3xl py-4">
@@ -60,30 +92,34 @@ const VolunteerInfoCard: React.FC<VolunteerInfoProps> = (info) => {
       </div>
 
       <div className="justify-start flex">
-        <div className="text-violet-500">{info.volName}&nbsp;</div>
+        <div className="text-violet-500">
+          {application.from.name ?? application.from.handle}&nbsp;
+        </div>
         <p> wants to work with your organization</p>
       </div>
       <div className="flex">
-        {info.avatar !== undefined && (
+        {application.from.coverPicture !== undefined && (
           <img
             className="rounded-sm py-3"
-            src={info.avatar}
+            src={getAvatar(application.from)}
             alt="Rounded avatar"
             style={{ width: '100px', height: 'auto' }}
           />
         )}
         <div className="flex justify-between py-3 pl-5">
           <div className="text-violet-500">bio:&nbsp;</div>
-          <p>{info.bio}</p>
+          <p>{application.from.bio}</p>
         </div>
       </div>
       <div className="flex">
         <div className="text-violet-500">location:&nbsp;</div>
-        <p>{info.location}</p>
+        {profileDataLoading ? <Spinner /> : <p>{location ?? ''}</p>}
+
+        {/* placeholder */}
       </div>
       <div className="flex">
         <div className="text-violet-500">Application date created:&nbsp;</div>
-        <p>{info.date}</p>
+        <p>{application.createdAt}</p>
       </div>
       <div className="flex mt-40">
         {' '}
@@ -99,89 +135,18 @@ const VolunteerInfoCard: React.FC<VolunteerInfoProps> = (info) => {
 }
 
 const VolunteerManagementTab: React.FC = () => {
+  const { currentUser: profile } = useAppPersistStore()
+
   const [searchValue, setSearchValue] = useState('')
   const [categories] = useState<Set<string>>(new Set())
   const [selectedCategory, setSelectedCategory] = useState<string>('')
-  const [selected, setSelected] = useState<number>()
+  const [selectedId, setSelectedId] = useState<string>('')
 
-  const testData = [
-    {
-      userName: 'May',
-      dateCreated: '6969',
-      location: 'france',
-      bio: 'im weird',
-      avatar:
-        'https://www.boredpanda.com/blog/wp-content/uploads/2021/06/Meet-Gertrude-the-duck-with-more-stylish-hair-than-yours-60d992b564567__700.jpg'
-    },
-    {
-      userName: 'bLAH',
-      dateCreated: '6969',
-      location: 'uk',
-      bio: 'bio 1',
-      avatar:
-        'https://blog.udemy.com/wp-content/uploads/2014/05/bigstock-test-icon-63758263.jpg'
-    },
-    {
-      userName: 'aksmc',
-      dateCreated: '6969',
-      location: 'canada',
-      bio: 'bio 2'
-    },
-    {
-      userName: 'May',
-      dateCreated: '6969',
-      location: 'usa',
-      bio: 'bio 3'
-    },
-    {
-      userName: 'lali',
-      dateCreated: '6969',
-      location: 'germany',
-      bio: 'bio 4'
-    },
-    {
-      userName: 'Michael',
-      dateCreated: '6969',
-      location: 'china',
-      bio: 'bio 5'
-    },
-    {
-      userName: 'johnny',
-      dateCreated: '6969',
-      location: 'korea',
-      bio: 'bio 6'
-    },
-    {
-      userName: 'lalalalaal',
-      dateCreated: '6969',
-      location: 'poland',
-      bio: 'bio 7'
-    },
-    {
-      userName: 'say less',
-      dateCreated: '6969',
-      location: 'mexico',
-      bio: 'bio 8'
-    },
-    {
-      userName: 'stop this',
-      dateCreated: '6969',
-      location: 'indonesia',
-      bio: 'bio 9'
-    },
-    {
-      userName: 'help',
-      dateCreated: '6969',
-      location: 'spain',
-      bio: 'bio 10'
-    },
-    {
-      userName: 'sry',
-      dateCreated: '6969',
-      location: 'egypt',
-      bio: 'bio 11'
-    }
-  ]
+  const { loading, data, error, refetch } = useApplications({ profile })
+
+  const selectedValue = useMemo(() => {
+    return data.find((val) => val.post_id === selectedId) ?? null
+  }, [data, selectedId])
 
   return (
     <GridLayout>
@@ -233,16 +198,17 @@ const VolunteerManagementTab: React.FC = () => {
         <div className="flex space-x-24 pt-5">
           <Card className={`h-1/2 w-1/2`}>
             <div className="scrollbar">
-              {testData.map((item, i) => {
+              {data.map((item) => {
                 return (
                   <PurpleBox
-                    key={i}
-                    selected={selected === i}
-                    userName={item.userName}
-                    dateCreated={item.dateCreated}
+                    key={item.post_id}
+                    selected={selectedId === item.post_id}
+                    userName={item.from.name ?? item.from.handle}
+                    dateCreated={item.createdAt}
                     onClick={() => {
-                      if (selected === i) setSelected(undefined)
-                      else setSelected(i)
+                      setSelectedId(
+                        selectedId === item.post_id ? '' : item.post_id
+                      )
                     }}
                   />
                 )
@@ -251,15 +217,9 @@ const VolunteerManagementTab: React.FC = () => {
               {/* the box placeholder for the data ^ */}
             </div>
           </Card>
-          {selected !== undefined && (
+          {selectedId !== '' && !!selectedValue && (
             <div className="pb-10">
-              <VolunteerInfoCard
-                volName={testData[selected].userName}
-                location={testData[selected].location}
-                date={testData[selected].dateCreated}
-                bio={testData[selected].bio}
-                avatar={testData[selected].avatar}
-              />
+              <VolunteerInfoCard application={selectedValue} />
             </div>
           )}
         </div>
