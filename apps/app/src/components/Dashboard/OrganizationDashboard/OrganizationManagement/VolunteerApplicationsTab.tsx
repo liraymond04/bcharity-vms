29237f1e868171c10259d548bb8c77901e59c96a
Ezra Huang <ezrahuang155@gmail.com>
2023-08-23 10:46:03 -0400

    fix layout structure

--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationManagement/VolunteerApplicationsTab.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationManagement/VolunteerApplicationsTab.tsx
@@ -1,11 +1,14 @@
+import { SearchIcon } from '@heroicons/react/outline'
 import { useMemo, useState } from 'react'
 
+import { ClearFilters } from '@/components/Shared'
 import { Card, ErrorMessage, Spinner } from '@/components/UI'
 import { checkAuth, useCreateComment } from '@/lib/lens-protocol'
 import useApplications from '@/lib/lens-protocol/useApplications'
 import { buildMetadata, PostTags } from '@/lib/metadata'
 import { useAppPersistStore } from '@/store/app'
 
+import { DashboardDropDown } from '../../VolunteerDashboard'
 import PurpleBox from './PurpleBox'
 import VolunteerApplicationCard from './VolunteerApplicationCard'
 
@@ -29,6 +32,10 @@ const VolunteerApplicationsTab: React.FC<IVolunteerApplicationsTabProps> = ({
 
   const [pendingIds, setPendingIds] = useState<Record<string, boolean>>({})
   const [verifyOrRejectError, setVerifyOrRejectError] = useState('')
+
+  const [searchValue, setSearchValue] = useState('')
+  const [categories] = useState<Set<string>>(new Set())
+  const [selectedCategory, setSelectedCategory] = useState<string>('')
 
   const setIdPending = (id: string) => {
     const newPendingIds = { ...pendingIds, [id]: true }
@@ -85,46 +92,84 @@ const VolunteerApplicationsTab: React.FC<IVolunteerApplicationsTabProps> = ({
 
   return (
     <div className={`${hidden ? 'hidden' : ''} flex space-x-24 pt-5`}>
-      <Card className={`h-1/2 w-1/2`}>
-        {(verifyOrRejectError || error) && (
-          <ErrorMessage error={new Error(verifyOrRejectError || error)} />
-        )}
-        <div className="scrollbar">
-          {loading ? (
-            <Spinner />
-          ) : (
-            <>
-              {data.map((item) => {
-                return (
-                  <PurpleBox
-                    key={item.post_id}
-                    selected={selectedId === item.post_id}
-                    userName={item.from.name ?? item.from.handle}
-                    dateCreated={item.createdAt}
-                    onClick={() => {
-                      setSelectedId(
-                        selectedId === item.post_id ? '' : item.post_id
-                      )
-                    }}
-                  />
-                )
-              })}
-            </>
-          )}
+      <div className="grow w-0">
+        <h1 className="text-3xl font-bold pb-3">Volunteer Applications</h1>
+        <div className="flex items-center">
+          <div className="flex justify-between h-[50px] bg-accent-content items-center rounded-md border-violet-300 border-2 dark:bg-Input">
+            <input
+              className="focus:ring-0 border-none outline-none focus:border-none focus:outline-none bg-transparent rounded-2xl"
+              type="text"
+              value={searchValue}
+              placeholder={'search'}
+              onChange={(e) => {
+                setSearchValue(e.target.value)
+              }}
+            />
+            <div className="h-5 w-5 mr-5">
+              <SearchIcon />
+            </div>
+          </div>
 
-          {/* the box placeholder for the data ^ */}
+          <div className="flex items-center">
+            <div className="h-[50px] z-10 ">
+              <DashboardDropDown
+                label={'filter'}
+                options={Array.from(categories)}
+                onClick={(c) => setSelectedCategory(c)}
+                selected={selectedCategory}
+              ></DashboardDropDown>
+            </div>
+            <ClearFilters
+              onClick={() => {
+                setSelectedCategory('')
+              }}
+            />
+          </div>
         </div>
-      </Card>
-      {selectedId !== '' && !!selectedValue && (
-        <div className="pb-10">
-          <VolunteerApplicationCard
-            application={selectedValue}
-            onAccept={() => onAcceptClick()}
-            onReject={() => onRejectClick()}
-            pending={pendingIds[selectedId]}
-          />
-        </div>
-      )}
+
+        <Card>
+          {(verifyOrRejectError || error) && (
+            <ErrorMessage error={new Error(verifyOrRejectError || error)} />
+          )}
+          <div className="scrollbar">
+            {loading ? (
+              <Spinner />
+            ) : (
+              <>
+                {data.map((item) => {
+                  return (
+                    <PurpleBox
+                      key={item.post_id}
+                      selected={selectedId === item.post_id}
+                      userName={item.from.name ?? item.from.handle}
+                      dateCreated={item.createdAt}
+                      onClick={() => {
+                        setSelectedId(
+                          selectedId === item.post_id ? '' : item.post_id
+                        )
+                      }}
+                    />
+                  )
+                })}
+              </>
+            )}
+
+            {/* the box placeholder for the data ^ */}
+          </div>
+        </Card>
+      </div>
+      <div className="grow w-0">
+        {selectedId !== '' && !!selectedValue && (
+          <div className="pb-10">
+            <VolunteerApplicationCard
+              application={selectedValue}
+              onAccept={() => onAcceptClick()}
+              onReject={() => onRejectClick()}
+              pending={pendingIds[selectedId]}
+            />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationManagement/VolunteerManagement.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationManagement/VolunteerManagement.tsx
@@ -1,10 +1,8 @@
-import { PlusCircleIcon, SearchIcon } from '@heroicons/react/outline'
+import { PlusCircleIcon } from '@heroicons/react/outline'
 import React, { useState } from 'react'
 
 import { GridItemTwelve, GridLayout } from '@/components/GridLayout'
-import ClearFilters from '@/components/Shared/ClearFilters'
 
-import DashboardDropDown from '../../VolunteerDashboard/DashboardDropDown'
 import AllVolunteersTab from './AllVolunteersTab'
 import VolunteerApplicationsTab from './VolunteerApplicationsTab'
 
@@ -17,59 +15,41 @@ import VolunteerApplicationsTab from './VolunteerApplicationsTab'
  * or {@link PostTags.Application.REJECT} metadata tag.
  */
 const VolunteerManagementTab: React.FC = () => {
-  const [searchValue, setSearchValue] = useState('')
-  const [categories] = useState<Set<string>>(new Set())
-  const [selectedCategory, setSelectedCategory] = useState<string>('')
+  const [openTab, setOpenTab] = useState(0)
+
+  const tabs = [
+    {
+      title: 'All Volunteers'
+    },
+    {
+      title: 'Volunteer Applications'
+    }
+  ]
 
   return (
     <GridLayout>
       <GridItemTwelve>
-        <div className="flex item-center my-10 px-10">
-          <div className="border-black bg-white text-black border-l px-2">
-            All Volunteers
-          </div>
-          <div className="border-black bg-white text-black border-l px-2">
-            Volunteers applications
-          </div>
-        </div>
-        <div className="flex flex-wrap gap-y-5 justify-around items-center mt-10">
-          <div className="flex justify-between w-[300px] h-[50px] bg-accent-content items-center rounded-md border-violet-300 border-2 ml-10 mr-10 dark:bg-Input">
-            <input
-              className="focus:ring-0 border-none outline-none focus:border-none focus:outline-none  bg-transparent rounded-2xl w-[250px]"
-              type="text"
-              value={searchValue}
-              placeholder={'search'}
-              onChange={(e) => {
-                setSearchValue(e.target.value)
-              }}
-            />
-            <div className="h-5 w-5 mr-5">
-              <SearchIcon />
-            </div>
-          </div>
-
-          <div className="flex flex-wrap gap-y-5 justify-around w-[420px] items-center">
-            <div className="h-[50px] z-10 ">
-              <DashboardDropDown
-                label={'filter'}
-                options={Array.from(categories)}
-                onClick={(c) => setSelectedCategory(c)}
-                selected={selectedCategory}
-              ></DashboardDropDown>
-            </div>
-            <ClearFilters
-              onClick={() => {
-                setSelectedCategory('')
-              }}
-            />
+        <div className="flex items-center mt-10 px-10">
+          {tabs.map((t, i) => (
+            <p
+              key={i}
+              className={`border-black text-black border-l px-2 ${
+                openTab === i ? 'bg-zinc-300' : 'bg-white'
+              }`}
+              onClick={() => setOpenTab(i)}
+            >
+              {t.title}
+            </p>
+          ))}
+          <div className="flex items-center justify-end pr-20 ml-auto">
+            <p>Add a Volunteer</p>
+            <PlusCircleIcon className="ml-2 w-8 text-brand-400" />
           </div>
         </div>
-        <div className="flex item-center justify-end pr-20 pt-10">
-          Add a Volunteer
-          <PlusCircleIcon className="w-8 text-brand-400" />
+        <div className="ml-5">
+          <VolunteerApplicationsTab hidden={openTab !== 0} />
+          <AllVolunteersTab hidden={openTab !== 1} />
         </div>
-        <VolunteerApplicationsTab hidden={false} />
-        <AllVolunteersTab hidden={false} />
       </GridItemTwelve>
     </GridLayout>
   )
