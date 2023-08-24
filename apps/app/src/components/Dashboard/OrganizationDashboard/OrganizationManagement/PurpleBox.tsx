5ff22ad93a6eb872b05d9a8b008d488331aef660
liraymond04 <li.raymond04@gmail.com>
2023-08-24 10:43:37 -0600

    Shorten date format for smaller screens

--- apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationManagement/PurpleBox.tsx
+++ apps/app/src/components/Dashboard/OrganizationDashboard/OrganizationManagement/PurpleBox.tsx
@@ -5,28 +5,46 @@ interface PurpleBoxProps {
   userName: string
   dateCreated: string
   onClick?: MouseEventHandler<HTMLDivElement>
+  tab?: string
 }
 
 const PurpleBox: React.FC<PurpleBoxProps> = ({
   selected,
   userName,
   dateCreated,
-  onClick
+  onClick,
+  tab
 }) => {
+  const fillZero = (n: number, w: number) => {
+    let str = String(n)
+    for (let i = str.length; i < w; i++) {
+      str = '0' + str
+    }
+    return str
+  }
+
+  const date = new Date(dateCreated)
+  const day = fillZero(date.getDay(), 2)
+  const month = fillZero(date.getMonth(), 2)
+  const year = fillZero(date.getFullYear(), 2)
+
   const boxClassName = selected
-    ? 'bg-blue-100 dark:bg-violet-500'
+    ? 'bg-blue-100 dark:bg-violet-500 scale-105'
     : 'bg-violet-200 dark:bg-Within dark:bg-opacity-10'
   return (
     <div
-      className={`items-center shadow-sm shadow-black px-5 mx-5 my-3 h-16 py-2 ${boxClassName}`}
+      className={`items-center shadow-sm shadow-black px-5 mx-5 my-3 h-16 py-2 hover:cursor-pointer transition duration-150 hover:scale-105 ${boxClassName}`}
       onClick={onClick}
     >
       <div>{userName}</div>
       <div className="flex justify-end my-3">
         <div className="text-sm font-extralight">
-          Application date created:&nbsp;
+          {tab === 'all' && 'Date joined:'}
+          {tab === 'applications' && 'Date created:'}&nbsp;
         </div>
-        <div className="text-sm font-extralight">{dateCreated}</div>
+        <div className="text-sm font-extralight">
+          {`${year}-${month}-${day}`}
+        </div>
       </div>
     </div>
   )
